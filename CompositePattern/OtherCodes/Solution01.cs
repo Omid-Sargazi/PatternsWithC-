5fa1f93e19fb9812990521acc754f174f59f82bb
Omid-Sargazi <om.sargazi@gmail.com>
2025-04-08 23:10:27 +0330

    sum two number and compare with target

---
+++ CompositePattern/OtherCodes/Solution01.cs
@@ -0,0 +1,20 @@
+namespace CompositePattern.Pattern01
+{
+    public class Solution01
+    {
+       public int[] TwoSum(int[] sums, int targer)
+       {
+        for(int i=0; i<sums.Length; i++)
+        {
+            for(int j = i+1; j<sums.Length; j++)
+            {
+                if(sums[i]+sums[j] == targer)
+                {
+                    return new int[] {i,j};
+                }
+                return new int[] {};
+            }
+        }
+       }
+    }
+}
