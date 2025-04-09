d279d012d5ce5f247872dc2533b99cb4e0865d4e
Omid-Sargazi <om.sargazi@gmail.com>
2025-04-09 19:07:34 +0330

    Length Of Longest Substring

--- LeetCode/Problems01/Solution.cs
+++ LeetCode/Problems01/Solution.cs
@@ -54,5 +54,28 @@ namespace LeetCode.Problems01
             }
             return length;
         }
+
+        public int LengthOfLongestSubstring(string s)
+        {
+            if(string.IsNullOrEmpty(s))
+            {
+                return 0;
+            }
+
+            Dictionary<char, int> charMap = new Dictionary<char, int>();
+            int maxLength = 0;
+            int start = 0;
+            for(int end = 0; end <= s.Length ; end++)
+            {
+                if(charMap.ContainsKey(s[end]) && charMap[s[end]]>=start)
+                {
+                    start = charMap[s[end]] + 1;
+                }
+                charMap[s[end]] = end;
+                int currentLength = end - start + 1;
+                maxLength = Math.Max(maxLength, currentLength);
+            }
+            return maxLength;
+        }
     }
 }
