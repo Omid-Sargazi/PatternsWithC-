63b27bb9f1242b0236ed6e3a95fae7640ee4617e
Omid-Sargazi <om.sargazi@gmail.com>
2025-04-11 00:09:35 +0330

    proxy pattern in cache database

--- CompositePattern/Database/CachingProductRepository.cs
+++ CompositePattern/Database/CachingProductRepository.cs
@@ -3,8 +3,13 @@ namespace CompositePattern.Database
 {
     public class CachingProductRepository : IProductRepository
     {
-        private DatabaseProductRepository _realRepo = new DatabaseProductRepository();
+        private DatabaseProductRepository _realRepo;
         private Dictionary<int, Product> _cache = new Dictionary<int, Product>();
+        private readonly object _lock = new object();
+        public CachingProductRepository(DatabaseProductRepository repository)
+        {
+            _realRepo = repository;
+        }
         public List<Product> GetAllProducts()
         {
             return _realRepo.GetAllProducts();
@@ -12,13 +17,26 @@ namespace CompositePattern.Database
 
         public Product GetProductById(int id)
         {
-            if(_cache.ContainsKey(id))
+            lock(_lock)
             {
-                Console.WriteLine($"Retrieved product {id} from cache");
-                return _cache[id];
+                if(_cache.ContainsKey(id))
+                {
+                    Console.WriteLine($"Retrieved product {id} from cache");
+                    return _cache[id];
+                }
+
             }
             var product = _realRepo.GetProductById(id);
+            lock (_lock)
+        {
+            _cache[id] = product;
+        }
             return product;
+        }
+        public void UpdateProduct(Product product)
+        {
+            Console.WriteLine($"Updated product {product.Id} in database");
+            _cache.Remove(product.Id); // کش رو invalidate کن
         }
     }
 }
