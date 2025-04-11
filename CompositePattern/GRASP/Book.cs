1d63484f0fdf9152c9f7631ed43cfa61eae27ae1
Omid-Sargazi <om.sargazi@gmail.com>
2025-04-11 13:14:11 +0330

    Information Expert in GRASP

--- CompositePattern/GRASP/Book.cs
+++ CompositePattern/GRASP/Book.cs
@@ -5,5 +5,26 @@ namespace CompositePattern.GRASP
         public string Title { get; set; }
         public string Author { get; set; }
         public bool IsBorrowed { get; set; }
+
+        public void Borrow()
+        {
+            if(!IsBorrowed)
+            {
+                IsBorrowed = true;
+                Console.WriteLine($"{Title} is now borrowed");
+            }
+            else{
+                Console.WriteLine($"{Title} is already borrowed");
+            }
+        }
+
+        public void Return()
+        {
+            if (IsBorrowed)
+            {
+                IsBorrowed = false;
+                Console.WriteLine($"{Title} is returned");
+            }
+        }
     }
 }
