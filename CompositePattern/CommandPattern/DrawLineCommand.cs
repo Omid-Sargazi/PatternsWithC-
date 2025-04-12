8e101621c8a463e07e222d5c3591676a9151fa14
Omid-Sargazi <om.sargazi@gmail.com>
2025-04-12 21:42:12 +0330

    create command

---
+++ CompositePattern/CommandPattern/DrawLineCommand.cs
@@ -0,0 +1,27 @@
+namespace CompositePattern.CommandPattern
+{
+    public class DrawLineCommand : IDrawCommand
+    {
+       private IDrawingMediator _mediator;
+    private int _x1, _y1, _x2, _y2;
+
+    public DrawLineCommand(IDrawingMediator mediator, int x1, int y1, int x2, int y2)
+    {
+        _mediator = mediator;
+        _x1 = x1;
+        _y1 = y1;
+        _x2 = x2;
+        _y2 = y2;
+    }
+
+    public void Execute()
+    {
+        _mediator.DrawShape("Line", _x1, _y1, _x2, _y2);
+    }
+
+    public void Undo()
+    {
+        _mediator.EraseShape("Line", _x1, _y1, _x2, _y2);
+    }
+    }
+}
