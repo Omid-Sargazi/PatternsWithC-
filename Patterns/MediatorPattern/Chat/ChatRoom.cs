967757c0ef02997a85323708a751b94e403f29c4
Omid-Sargazi <om.sargazi@gmail.com>
2025-04-15 21:56:13 +0330

    define a mediator chat room

--- Patterns/MediatorPattern/Chat/ChatRoom.cs
+++ Patterns/MediatorPattern/Chat/ChatRoom.cs
@@ -2,9 +2,26 @@ namespace Patterns.MediatorPattern.Chat
 {
     public class ChatRoom : IMediator
     {
+        private ComponentA _componentA;
+        private ComponentB _componentB;
+        public ChatRoom(ComponentA componentA, ComponentB componentB)
+        {
+            _componentA = componentA;
+            _componentB = componentB;
+            _componentA.SetMediator(this);
+            _componentB.SetMediator(this);
+        }
+        
         public void Notify(object sender, string message)
         {
-            throw new NotImplementedException();
+            if(sender == _componentA)
+            {
+                _componentB.ReceiveMessage(message);
+            }
+            else if(sender == _componentB)
+            {
+                _componentA.ReceiveMessage(message);
+            }
         }
     }
 }
