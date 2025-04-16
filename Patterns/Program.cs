dd2ac5d5d4cd4d08540ab80f0c433febe3398448
Omid-Sargazi <om.sargazi@gmail.com>
2025-04-16 23:50:47 +0330

    insatll rabbitMQ

--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -8,23 +8,24 @@ public class Program
     public static void Main(string[] args)
     {
         Console.WriteLine("hello");
-        var media = new MediaPlayer();
-        media.Paly("mp3", "song.mp3");
-        media.Paly("wav", "moderntalking.wav");
+        // var media = new MediaPlayer();
+        // media.Paly("mp3", "song.mp3");
+        // media.Paly("wav", "moderntalking.wav");
 
-        var menu = new Menu("Main Course");
-        menu.Add(new MenuItem("Grilled Chicken",15));
-        var subMenu = new Menu("Vegetarian");
-        menu.Add(subMenu);
-        subMenu.Add(new MenuItem("Paneer Tikka", 12));
-        subMenu.Add(new MenuItem("Veg Biryani", 10));
-        var subMenu02 = new Menu("Non-Vegetarian");
-        menu.Add(subMenu02);
-        subMenu02.Add(new MenuItem("Chicken Biryani",14));
-        menu.Display();
+        // var menu = new Menu("Main Course");
+        // menu.Add(new MenuItem("Grilled Chicken",15));
+        // var subMenu = new Menu("Vegetarian");
+        // menu.Add(subMenu);
+        // subMenu.Add(new MenuItem("Paneer Tikka", 12));
+        // subMenu.Add(new MenuItem("Veg Biryani", 10));
+        // var subMenu02 = new Menu("Non-Vegetarian");
+        // menu.Add(subMenu02);
+        // subMenu02.Add(new MenuItem("Chicken Biryani",14));
+        // menu.Display();
 
-        Television tv = new Television();
-        Patterns.CommandPattern.WithPattern.RemoteController remote = new Patterns.CommandPattern.WithPattern.RemoteController(new TurnOnOperation(tv), new TurnOffOperation(tv));
-        tv.
+        // Television tv = new Television();
+        // Patterns.CommandPattern.WithPattern.RemoteController remote = new Patterns.CommandPattern.WithPattern.RemoteController(new TurnOnOperation(tv), new TurnOffOperation(tv));
+        
+        
     }
 }
--- Patterns/obj/Debug/net9.0/Patterns.AssemblyInfo.cs
+++ Patterns/obj/Debug/net9.0/Patterns.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Patterns")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aea3e3675485e8c087bf1db885dd7d30ec801483")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e59666a8c2d2aa851e813b68844b35ee0d63e69")]
 [assembly: System.Reflection.AssemblyProductAttribute("Patterns")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Patterns")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
