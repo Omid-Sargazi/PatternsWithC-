1c033a09676e81a0bac6c89f0d47afa500f4f5ed
Omid-Sargazi <om.sargazi@gmail.com>
2025-04-19 22:10:06 +0330

    define a model for emails

---
+++ SendEmail/Model/EmailModel.cs
@@ -0,0 +1,9 @@
+namespace SendEmail.Model
+{
+    public class EmailModel
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
