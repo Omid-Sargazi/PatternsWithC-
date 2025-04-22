86121e00ce4712fd13a70cfedc44609b78c3cfe0
Omid-Sargazi <om.sargazi@gmail.com>
2025-04-22 23:50:25 +0330

    define draft class

--- Projects01/RabbitMQ/Document/DraftState.cs
+++ Projects01/RabbitMQ/Document/DraftState.cs
@@ -4,17 +4,25 @@ namespace RabbitMQ.Document
     {
         public void Approve(Document context, Role role)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("سند در حالت پیش‌نویس است و نمی‌تواند تأیید شود!");
         }
 
         public void Archive(Document context, Role role)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("سند در حالت پیش‌نویس است و نمی‌تواند آرشیو شود!");
         }
 
         public void Delete(Document context, Role role)
         {
-            throw new NotImplementedException();
+            if (role == Role.Author)
+            {
+                Console.WriteLine("سند حذف شد.");
+                // برای سادگی فرض می‌کنیم سند حذف می‌شه و دیگه حالتی نداره
+            }
+            else
+            {
+                Console.WriteLine("فقط نویسندگان می‌توانند سند را حذف کنند!");
+            }
         }
 
         public void Edit(Document context, Role role)
@@ -31,12 +39,12 @@ namespace RabbitMQ.Document
 
         public void Reject(Document context, Role role)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("سند در حالت پیش‌نویس است و نمی‌تواند رد شود!");
         }
 
         public void Restore(Document context, Role role)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("سند در حالت پیش‌نویس است و نیازی به بازیابی ندارد!");
         }
 
         public void SubmitForModeration(Document context, Role role)
@@ -45,6 +53,10 @@ namespace RabbitMQ.Document
             {
                 Console.WriteLine("سند برای بررسی ارسال شد.");
                 context.SetState(new ModerationState());
+            }
+            else
+            {
+                Console.WriteLine("فقط نویسندگان می‌توانند سند را برای بررسی ارسال کنند!");
             }
         }
     }
