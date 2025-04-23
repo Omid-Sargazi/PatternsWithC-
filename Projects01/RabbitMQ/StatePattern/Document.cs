65c82d0d29794d6a9bf997788554756300555119
Omid-Sargazi <om.sargazi@gmail.com>
2025-04-23 10:38:54 +0330

    project without status pattern

---
+++ Projects01/RabbitMQ/StatePattern/Document.cs
@@ -0,0 +1,27 @@
+namespace RabbitMQ.StatePattern
+{
+    public class Document
+    {
+        public enum Status {Draft, Moderation, Published}
+        private Status _status = Status.Draft;
+
+        public void Publish()
+        {
+            if(_status == Status.Draft)
+                _status = Status.Published;
+            else 
+                throw new InvalidOperationException("Document is not in draft state.");
+        }
+
+        public void Render()
+        {
+            if(_status == Status.Draft)
+            {
+                Console.WriteLine("Editing view");
+            }else if(_status == Status.Moderation)
+                Console.WriteLine("Preview view");
+            else
+                Console.WriteLine("Read-only view");
+        }
+    }
+}
--- Projects01/RabbitMQ/obj/Debug/net9.0/RabbitMQ.AssemblyInfo.cs
+++ Projects01/RabbitMQ/obj/Debug/net9.0/RabbitMQ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RabbitMQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a52522b36cc6521bbf81142085f450c960c6bae2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86121e00ce4712fd13a70cfedc44609b78c3cfe0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RabbitMQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RabbitMQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
