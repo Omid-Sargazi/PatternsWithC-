56556273d35f9d8defd226961f896c0f505100f4
Omid-Sargazi <om.sargazi@gmail.com>
2025-04-27 21:21:06 +0330

    video stream by proxy pattern

--- Projects01/PatternsInCSharp/ProxyPattern/VideoStreamer/Video.cs
+++ Projects01/PatternsInCSharp/ProxyPattern/VideoStreamer/Video.cs
@@ -19,22 +19,65 @@ namespace PatternsInCSharp.ProxyPattern.VideoStreamer
         }
     }
 
-    public class VideoStreamer
+    public interface IVideoStreamer
+    {
+        string GetTitle();
+        int GetDuration();
+        string StreamVideo();
+    }
+
+    public class VideoStreamer : IVideoStreamer
     {
         private readonly string _videoId;
         private Video _video;
         public VideoStreamer(string videoId)
         {
             _videoId = videoId;
-            _video = LoadVideoFromServer(videoId);
+            
         }
 
 
-        private Video LoadVideoFromServer(string videoId)
+        private Video LoadVideoFromServer()
         {
-            Console.WriteLine($"Loading video {videoId} from server...");
+            Console.WriteLine($"Loading video {_videoId} from server...");
             Thread.Sleep(3000); // ۳ ثانیه تاخیر
             return new Video("Sample Video", 120, "Heavy 4K video data...");
+        }
+
+        public string GetTitle()
+        {
+            _video ??= LoadVideoFromServer();
+            return _video.Title;
+        }
+
+        public int GetDuration()
+        {
+            _video ??= LoadVideoFromServer();
+            return _video.DurationInSeconds;
+        }
+
+        public string StreamVideo()
+        {
+            _video ??= LoadVideoFromServer();
+            return _video.VideoDate;
+        }
+    }
+
+    public class VideoProxyStreamer : IVideoStreamer
+    {
+        private Video _video;
+        private readonly string _videoId;
+        private VideoStreamer _videoStreamer;
+        private readonly bool _isVipUser;
+        public VideoProxyStreamer(string videoId, bool isVipUser)
+        {
+            _videoId = videoId;
+            _isVipUser = isVipUser;
+            _video = new Video("Sample Video",120, null);
+        }
+        public int GetDuration()
+        {
+            return _video.DurationInSeconds;
         }
 
         public string GetTitle()
@@ -42,14 +85,14 @@ namespace PatternsInCSharp.ProxyPattern.VideoStreamer
             return _video.Title;
         }
 
-        public int GetDuration()
-        {
-            return _video.DurationInSeconds;
-        }
-
         public string StreamVideo()
         {
-            return _video.VideoDate;
+            if(!_isVipUser)
+            {
+                return "You need to be a VIP user to stream this video.";
+            }
+            _videoStreamer ??= new VideoStreamer(_videoId);
+            return _videoStreamer.StreamVideo();
         }
     }
 }
