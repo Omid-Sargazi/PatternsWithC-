3c6c2c3d0bf83a0070deca56ec5d3ded54c1ef68
Omid-Sargazi <om.sargazi@gmail.com>
2025-04-30 10:51:17 +0330

    answer to medical questions and access to real doctor with proxy pattern

--- Projects01/PatternsInCSharp02/ProxyPattern/RealDoctor.cs
+++ Projects01/PatternsInCSharp02/ProxyPattern/RealDoctor.cs
@@ -1,7 +1,7 @@
 
 namespace PatternsInCSharp02.ProxyPattern
 {
-    public class RealDoctor
+    public class RealDoctor : IDoctor
     {
         public string AnswerMedicalQuestion(string question)
         {
@@ -13,11 +13,57 @@ namespace PatternsInCSharp02.ProxyPattern
 
     public class MedicalApp
     {
-        public void HandleUserRequest(string question)
+        private IDoctor _doctor;
+
+        public MedicalApp(IDoctor doctor)
         {
-            var doctor = new RealDoctor();
-            var answer = doctor.AnswerMedicalQuestion(question);
-            Console.WriteLine(answer);
+            _doctor = doctor;
+        }
+        public void Consult(string question)
+        {
+           Console.WriteLine($"\nسوال کاربر: {question}");
+           var answer = _doctor.AnswerMedicalQuestion(question);
+           Console.WriteLine($"پاسخ سیستم: {answer}");
+        }
+    }
+
+    public interface IDoctor
+    {
+        string AnswerMedicalQuestion(string question);
+    }
+
+    public class DoctorProxy : IDoctor
+    {
+        private RealDoctor _realDoctor;
+        private Dictionary<string, string> _commonQuestionsCache = new()
+        {
+            {"سردرد", "استراحت کنید و مایعات بنوشید"},
+        {"تب", "استامینوفن مصرف کنید و استراحت نمایید"}
+        };
+
+        public string AnswerMedicalQuestion(string question)
+        {
+            if(_commonQuestionsCache.ContainsKey(question))
+            {
+                Console.WriteLine("The answer is in the chache");
+                return _commonQuestionsCache[question];
+            }
+            if(!CheckUserAccess())
+            {
+                return "برای مشاوره تخصصی باید اشتراک ویژه داشته باشید";
+            }
+            if(_realDoctor == null)
+            {
+                _realDoctor = new RealDoctor();
+            }
+
+            return _realDoctor.AnswerMedicalQuestion(question);
+        }
+
+        private bool CheckUserAccess()
+        {
+            Console.WriteLine("بررسی سطح دسترسی کاربر...");
+            return new Random().Next(0, 2) == 1; // تصادفی true/false
         }
     }
 }
