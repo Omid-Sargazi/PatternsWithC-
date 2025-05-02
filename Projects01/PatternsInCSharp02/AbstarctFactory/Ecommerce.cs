bfa0b8dee4f0e9082b941bce2e8f363b718bd456
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-02 20:05:49 +0330

    complete abstarct factoy and usage of it

--- Projects01/PatternsInCSharp02/AbstarctFactory/Ecommerce.cs
+++ Projects01/PatternsInCSharp02/AbstarctFactory/Ecommerce.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using System.Net.NetworkInformation;
 
 namespace PatternsInCSharp02.AbstarctFactory
@@ -75,5 +76,130 @@ namespace PatternsInCSharp02.AbstarctFactory
             Console.WriteLine($"Processing payment with Square: ${amount}");
             return true;
         }
+
     }
+    public class FedExShipping : IShippingProvider
+    {
+        private readonly Lazy<object> _apiClient;
+        public FedExShipping()
+        {
+            _apiClient = new Lazy<object>(()=>{
+                Console.WriteLine("Initializing FedEx API client - expensive operation");
+                return new object();
+            });
+        }
+        public string ArrangeShipping(string address, double weight)
+        {
+            var client = _apiClient.Value;
+            Console.WriteLine($"Arranging FedEx shipping to: {address} with weight: {weight}");
+            return $"FEDEX-{Guid.NewGuid().ToString().Substring(0, 8)}";
+        }
+    }
+
+    public class UPSShipping : IShippingProvider
+    {
+        private readonly Lazy<object> _apiClient;
+        public UPSShipping()
+        {
+            _apiClient = new Lazy<object>(()=>
+            {
+                Console.WriteLine("Initializing UPS API client - expensive operation");
+                return new object();
+            });
+        }
+        public string ArrangeShipping(string address, double weight)
+        {
+            var client = _apiClient.Value;
+            Console.WriteLine($"Arranging UPS shipping to: {address} with weight: {weight}");
+            return $"UPS-{Guid.NewGuid().ToString().Substring(0, 8)}";
+        }
+    }
+
+    public class DHLShipping : IShippingProvider
+    {
+        private readonly Lazy<object> _apiClient;
+        public DHLShipping()
+        {
+            _apiClient = new Lazy<object>(()=>
+            {
+                Console.WriteLine("Initializing DHL API client - expensive operation");
+                return new object();
+            });
+        }
+        public string ArrangeShipping(string address, double weight)
+        {
+            var client = _apiClient.Value;
+            Console.WriteLine($"Arranging DHL shipping to: {address} with weight: {weight}");
+            return $"DHL-{Guid.NewGuid().ToString().Substring(0, 8)}";
+        }
+    }
+
+    public interface ICommerceFactory
+    {
+        IPaymentProcessor CreatePaymentProcessor();
+        IShippingProvider CreateShippingProvider();
+    }
+
+    public class PayPalFedExFactory : ICommerceFactory
+    {
+        public IPaymentProcessor CreatePaymentProcessor()
+        {
+            return new PayPalProcessor();
+        }
+
+        public IShippingProvider CreateShippingProvider()
+        {
+            return new FedExShipping();
+        }
+    }
+
+    public class PayPalUPSFactory : ICommerceFactory
+    {
+        public IPaymentProcessor CreatePaymentProcessor()
+        {
+            return new PayPalProcessor();
+        }
+
+        public IShippingProvider CreateShippingProvider()
+        {
+            return new UPSShipping();
+        }
+    }
+
+    public class PayPalDHLFactory : ICommerceFactory
+    {
+        public IPaymentProcessor CreatePaymentProcessor()
+        {
+            return new PayPalProcessor();
+        }
+
+        public IShippingProvider CreateShippingProvider()
+        {
+            return new DHLShipping();
+        }
+    }
+
+    public static class CommerceObjectPool
+    {
+        private static readonly ConcurrentDictionary<Type, IPaymentProcessor> PaymentProcessors = new ConcurrentDictionary<Type, IPaymentProcessor>();
+        private static readonly ConcurrentDictionary<Type, IShippingProvider> ShippingProviders = new ConcurrentDictionary<Type, IShippingProvider>();
+
+        public static T GetPaymentProcessor<T>() where T : IPaymentProcessor, new()
+        {
+             return (T)PaymentProcessors.GetOrAdd(typeof(T), _ => new T());
+        }
+
+        public static T GetShippingProvider<T>() where T : IShippingProvider, new()
+        {
+            return (T)ShippingProviders.GetOrAdd(typeof(T), _ => new T());
+        }
+        
+        // For testing purposes - clear the pools
+        public static void ClearPools()
+        {
+            PaymentProcessors.Clear();
+            ShippingProviders.Clear();
+        }
+    }
+
 }
