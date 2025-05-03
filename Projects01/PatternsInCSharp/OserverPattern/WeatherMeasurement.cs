a7053412497c18af036377d50868ba5187267a9d
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-03 18:59:50 +0330

    define Weather data  on base observer pattern

--- Projects01/PatternsInCSharp/OserverPattern/WeatherMeasurement.cs
+++ Projects01/PatternsInCSharp/OserverPattern/WeatherMeasurement.cs
@@ -1,3 +1,5 @@
+using System.Net.NetworkInformation;
+
 namespace PatternsInCSharp.ObserverPattern
 {
     public class WeatherMeasurement
@@ -27,6 +29,69 @@ namespace PatternsInCSharp.ObserverPattern
         public override int GetHashCode()
         {
             return HashCode.Combine(Temperature,Humidity, Pressure);
+        }
+    }
+    public interface IWeatherObserver
+    {
+        HashSet<WeatherDataType> InterestedDataTypes {get;}
+        void Update(WeatherMeasurement measurement);
+    }
+
+    public enum WeatherDataType
+    {
+        Temperature,
+        Humidity,
+        Pressure
+    }
+
+
+    public class WeatherData
+    {
+        private WeatherMeasurement _lastMeasurement;
+        private readonly object _lock = new object();
+        private readonly List<IWeatherObserver> _observers = new List<IWeatherObserver>();
+
+
+        public void RegisterObserver(IWeatherObserver observer)
+        {
+            lock(_lock)
+            {
+                _observers.Add(observer);
+            }
+        }
+
+        public void RemoveObserver(IWeatherObserver observer)
+        {
+            lock(_lock)
+            {
+                _observers.Remove(observer);
+            }
+        }
+
+        private void NotifyObservers(WeatherMeasurement measurement)
+        {
+            IWeatherObserver[] observersSnapshot;
+            lock(_lock)
+            {
+                observersSnapshot = _observers.ToArray();
+            }
+
+            Parallel.ForEach(observersSnapshot, observer=>{
+                bool shouldNotify = observer.InterestedDataTypes.Any(datatype=> 
+                datatype switch
+                {
+                    WeatherDataType.Temperature => measurement.Temperature != _lastMeasurement.Temperature,
+                    WeatherDataType.Humidity => measurement.Humidity != _lastMeasurement?.Humidity,
+                    WeatherDataType.Pressure => measurement.Pressure != _lastMeasurement?.Pressure,
+                    _ => false
+                }
+                );
+
+                if(shouldNotify)
+                {
+                    observer.Update(measurement);
+                }
+            });
         }
     }
 }
