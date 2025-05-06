3152efc6edc5859cdf08571799f3f652c891a49b
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-07 01:11:39 +0330

    implement Financial Document by Visitor Pattern

---
+++ Projects01/DesignPatterns/BehavioralPattern/VisitorPattern/DocumentType.cs
@@ -0,0 +1,139 @@
+namespace BehavioralPattern.VisitorPattern
+{
+    public enum DocumentType
+    {
+        Invoice,
+        Receipt,
+        Contract,
+        Payroll,
+    }
+
+    public interface IFinancialDocumentVisitor
+    {
+        void Visit(Invoice visitor);
+        void Visit(Receipt visitor);
+        void Visit(Contract visitor);
+        void Visit(Payroll visitor);
+    }
+
+    public abstract class FinancialDocument
+    {
+        public string Id {get; set;}
+        public DateTime Date {get; set;}
+        public decimal Amount {get; set;}
+        public DocumentType Type {get; set;}
+        public abstract void Accept(IFinancialDocumentVisitor visitor);
+    }
+
+    public class Invoice : FinancialDocument
+    {
+        public string CustomerName { get; set; }
+        public List<string> Items { get; set; }
+        public bool IsPaid { get; set; }
+        public override void Accept(IFinancialDocumentVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+    public class Receipt : FinancialDocument
+    {
+         public string PaymentMethod { get; set; }
+        public string ReceivedFrom { get; set; }
+        public override void Accept(IFinancialDocumentVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public class Contract : FinancialDocument
+    {
+         public string PartyName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Terms { get; set; }
+        public override void Accept(IFinancialDocumentVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public class Payroll : FinancialDocument
+    {
+        public string EmployeeName { get; set; }
+        public int WorkingHours { get; set; }
+        public decimal TaxAmount { get; set; }
+        public override void Accept(IFinancialDocumentVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public class GenerateDetailReportVisitor : IFinancialDocumentVisitor
+    {
+        public void Visit(Invoice visitor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Receipt visitor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Contract visitor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Payroll visitor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class GenerateSummaryReport : IFinancialDocumentVisitor
+    {
+        public void Visit(Invoice visitor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Receipt visitor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Contract visitor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Payroll visitor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class GenerateTaxReportVisitor : IFinancialDocumentVisitor
+    {
+        public void Visit(Invoice visitor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Receipt visitor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Contract visitor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Payroll visitor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
