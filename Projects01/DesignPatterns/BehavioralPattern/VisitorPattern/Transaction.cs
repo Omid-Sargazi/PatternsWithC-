90171ceae6d1800f22c3e50dec4fdf8768f74fc5
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-07 00:41:08 +0330

    useage of pattern

--- Projects01/DesignPatterns/BehavioralPattern/VisitorPattern/Transaction.cs
+++ Projects01/DesignPatterns/BehavioralPattern/VisitorPattern/Transaction.cs
@@ -21,6 +21,8 @@ namespace BehavioralPattern.VisitorPattern
 
     public class Purchase : Transaction
     {
+        public string Vendor {get; set;}
+        public string ProductCategory {get; set;}
         public override void Accept(ITransactionVisitor visitor)
         {
             visitor.Visit(this);
@@ -29,6 +31,8 @@ namespace BehavioralPattern.VisitorPattern
 
     public class Sale : Transaction
     {
+        public string Customer {get; set;}
+        public decimal Profit {get; set;}
         public override void Accept(ITransactionVisitor visitor)
         {
             visitor.Visit(this);
@@ -37,6 +41,8 @@ namespace BehavioralPattern.VisitorPattern
 
     public class Transfer : Transaction
     {
+        public string FromAccount {get; set;}
+        public string ToAccount {get; set;}
         public override void Accept(ITransactionVisitor visitor)
         {
             visitor.Visit(this);
@@ -45,6 +51,9 @@ namespace BehavioralPattern.VisitorPattern
 
     public class BillPayment : Transaction
     {
+         public string Payee { get; set; }
+        public string BillType { get; set; }
+        public string BillNumber { get; set; }
         public override void Accept(ITransactionVisitor visitor)
         {
             visitor.Visit(this);
@@ -70,7 +79,8 @@ namespace BehavioralPattern.VisitorPattern
 
         public string Visit(BillPayment visitor)
         {
-            throw new NotImplementedException();
+            return $"مالیات پرداخت قبض: {visitor.Date} به {visitor.Payee} برای {visitor.BillType} - " + 
+                   $"شماره قبض: {visitor.BillNumber} - مبلغ: {visitor.Amount}";
         }
     }
 
