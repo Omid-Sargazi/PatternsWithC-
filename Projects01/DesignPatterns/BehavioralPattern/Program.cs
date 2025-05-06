1b8afb53ba4c5648778c250e04b64eb2f663c329
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-06 20:52:39 +0330

    usage of visitor pattern

--- Projects01/DesignPatterns/BehavioralPattern/Program.cs
+++ Projects01/DesignPatterns/BehavioralPattern/Program.cs
@@ -7,6 +7,26 @@ public class Program
         // See https://aka.ms/new-console-template for more information
         Console.WriteLine("Hello, World!");
 
+        var document = new List<Document>
+        {
+            new TextDocument {Name = "ReadMe",Content = "This is a sample document", CreatedDate = DateTime.Now},
+            new PdfDocument { Name = "Report", Content = "Pdf content", CreatedDate = DateTime.Now},
+            new ImageDocument {Name="Logo", pixel="1986,1523", CreatedDate = DateTime.Now}
+        };
+
+        var xmlExporter = new XmlExportVisitor();
+        var JsonExport = new JsonExportVisitor();
+        var plainTextExport = new PlainTextExportVisitor();
+
+        foreach(var item in document)
+        {
+            item.Accept(xmlExporter);
+        }
+
+        foreach(var item in document)
+        {
+            item.Accept(JsonExport);
+        }
         
       
     }
