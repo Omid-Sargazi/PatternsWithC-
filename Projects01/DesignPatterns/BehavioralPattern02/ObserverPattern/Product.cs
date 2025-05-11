cca1146e8c2431576634fb2ecf14fb780e1aa42f
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-11 22:25:13 +0330

    define a primary system for announcement between customer and product

---
+++ Projects01/DesignPatterns/BehavioralPattern02/ObserverPattern/Product.cs
@@ -0,0 +1,37 @@
+namespace BehavioralPattern02.ObserverPattern
+{
+    public class Product
+    {
+        public string name {get; set;}
+        public bool isStick {get; set;}
+        public Customer customer;
+
+        public void setCustomer(Customer customer)
+        {
+            this.customer = customer;
+        }
+
+        public void setInStock(bool inStock)
+        {
+            if(inStock && customer !=null)
+            {
+                customer.Update(this.name+"there is now");
+            }
+        }
+    }
+
+    public class Customer
+    {
+        private string Name {get; set;}
+
+        public Customer(string name)
+        {
+            Name = name;
+        }
+
+        public void Update(string message)
+        {
+            Console.WriteLine($"{Name} received {message}");
+        }
+    }
+}
--- Projects01/DesignPatterns/BehavioralPattern02/obj/Debug/net9.0/BehavioralPattern02.AssemblyInfo.cs
+++ Projects01/DesignPatterns/BehavioralPattern02/obj/Debug/net9.0/BehavioralPattern02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BehavioralPattern02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+238aeceb08c1f83171fb33df65f1228291197583")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c5bdb0236b8fd64ecca6a0070a4cb7f038f981f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BehavioralPattern02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BehavioralPattern02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
