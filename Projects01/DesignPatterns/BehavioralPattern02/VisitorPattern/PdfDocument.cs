aa065d38ebaf898c53aa716b168e51b4e000f6b5
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-11 22:03:45 +0330

    .

--- Projects01/DesignPatterns/BehavioralPattern02/VisitorPattern/PdfDocument.cs
+++ Projects01/DesignPatterns/BehavioralPattern02/VisitorPattern/PdfDocument.cs
@@ -2,9 +2,7 @@ namespace BehavioralPattern02.VisitorPattern
 {
     public abstract class Document
     {
-        public abstract void DisplayInfo();
-        public abstract double CalculateCompressedSize();
-        public abstract string ConvertFormat();
+        
     }
 
     public class PdfDocument : Document
@@ -15,20 +13,7 @@ namespace BehavioralPattern02.VisitorPattern
         {
             PageCount = pageCount;
         }
-        public override double CalculateCompressedSize()
-        {
-            return PageCount * 0.1 * 0.5;
-        }
-
-        public override void DisplayInfo()
-        {
-            Console.WriteLine($"PDF Document with {PageCount} pages");
-        }
-
-        public override string ConvertFormat()
-        {
-            return "Converted PDF to Word format";
-        }
+      
     }
 
     public class WordDocument : Document
@@ -39,20 +24,7 @@ namespace BehavioralPattern02.VisitorPattern
         {
             WordCount = wordCount;
         }
-        public override double CalculateCompressedSize()
-        {
-           return (WordCount / 1000.0) * 0.2 * 0.5;
-        }
-
-        public override void DisplayInfo()
-        {
-            Console.WriteLine($"Word Document with {WordCount} words");
-        }
-
-        public override string ConvertFormat()
-        {
-            return "Converted Word to PDF format";
-        }
+       
     }
 
     public class DocumentOperations
