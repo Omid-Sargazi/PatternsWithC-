cac692bfa246d07147dbcc0158bba91bbf8e5d74
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-11 22:03:00 +0330

    Refactor: Move operations to DocumentOperations class - Removed operations from Document hierarchy - Added DocumentOperations class to handle display, compression, and conversion - Used type checking to handle different documents

--- Projects01/DesignPatterns/BehavioralPattern02/VisitorPattern/PdfDocument.cs
+++ Projects01/DesignPatterns/BehavioralPattern02/VisitorPattern/PdfDocument.cs
@@ -55,4 +55,50 @@ namespace BehavioralPattern02.VisitorPattern
         }
     }
 
+    public class DocumentOperations
+    {
+        public void DisplayInfo(Document doc)
+        {
+            if(doc is PdfDocument pdfDocument)
+            {
+                Console.WriteLine($"PDF Document with {pdfDocument.PageCount} pages");
+            }
+            else if(doc is WordDocument wordDocument)
+            {
+                Console.WriteLine($"Word Document with {wordDocument.WordCount} words");
+            }
+            else
+            {
+                throw new NotSupportedException("Document not supported");
+            }
+        }
+
+        public double CalculateCompressedSize(Document doc)
+        {
+            if(doc is PdfDocument pdfDocument)
+            {
+                return pdfDocument.PageCount * 0.1 * 0.5;
+            }
+            else if(doc is WordDocument wordDocument)
+            {
+                return (wordDocument.WordCount / 1000.0) * 0.2 * 0.5;
+            }
+            throw new NotSupportedException("Document not supported");
+        }
+
+        public string ConvertFormat(Document doc)
+        {
+            if(doc is PdfDocument)
+            {
+                return "Converted PDF to Word format";
+            }
+            else if(doc is WordDocument)
+            {
+                return "Converted Word to PDF format";
+            }
+
+            throw new NotSupportedException("Document not supported");
+        }
+    }
+
 }
