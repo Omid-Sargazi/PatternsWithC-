0f379171ecca0d427f8d7bfa6fd38d325289662e
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-12 20:07:47 +0330

    Added hardcoded Mobile and Web displays to update on stock price change

--- Projects01/DesignPatterns/BehavioralPattern02/ObserverPattern/Stock.cs
+++ Projects01/DesignPatterns/BehavioralPattern02/ObserverPattern/Stock.cs
@@ -5,15 +5,39 @@ namespace BehavioralPattern02.ObserverPattern
         public string Symbol {get; set;}
         public decimal Price {get; private set;}
 
+        private MobileDisplay mobileDisplay;
+        private WebDisplay webDisplay;
+
         public Stock(string symbol, decimal price)
         {
             Symbol = symbol;
             Price = price;
+
+            mobileDisplay = new MobileDisplay();
+            webDisplay = new WebDisplay();
         }
 
         public void SetPrice(decimal price)
         {
             Price = price;
+            mobileDisplay.ShowPrice(Symbol, Price);
+            webDisplay.ShowPrice(Symbol, Price);
+        }
+    }
+
+    public class MobileDisplay
+    {
+        public void ShowPrice(string symbol, decimal price)
+        {
+            Console.WriteLine($"[Mobile] {symbol} is now {price}");
+        }
+    }
+
+    public class WebDisplay
+    {
+        public void ShowPrice(string symbol, decimal price)
+        {
+            Console.WriteLine($"[Web] {symbol} is now {price}");
         }
     }
 }
--- Projects01/DesignPatterns/BehavioralPattern02/Program.cs
+++ Projects01/DesignPatterns/BehavioralPattern02/Program.cs
@@ -5,8 +5,10 @@ public class Program
     public static void Main(string[] args)
     {
         var stock = new Stock("MSFT",300.00m);
-        Console.WriteLine($"Stock: {stock.Symbol}, Price: {stock.Price}");
-        stock.SetPrice(305.05m);
-        Console.WriteLine($"Stock: {stock.Symbol}, Price: {stock.Price}");
+        // Console.WriteLine($"Stock: {stock.Symbol}, Price: {stock.Price}");
+        // stock.SetPrice(305.05m);
+        // Console.WriteLine($"Stock: {stock.Symbol}, Price: {stock.Price}");
+
+        stock.SetPrice(155.03m);
     }
 }
