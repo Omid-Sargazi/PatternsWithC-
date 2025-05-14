3fdd39c3bfbd3c870107d4abe6e78478da2d9619
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-14 19:07:11 +0330

    Applied Bridge pattern: decoupled Document abstraction from Renderer implementation

--- Projects01/DesignPatterns/BehavioralPattern02/BridgePattern/Invoice.cs
+++ Projects01/DesignPatterns/BehavioralPattern02/BridgePattern/Invoice.cs
@@ -1,5 +1,15 @@
 namespace BehavioralPattern02.BridgePattern
 {
+    public abstract class Documentt
+    {
+        protected readonly IDocRenderer _renderer;
+        protected Documentt(IDocRenderer renderer)
+        {
+            _renderer = renderer;
+        }
+
+        public abstract void Render();
+    }
     public interface IDocRenderer
     {
         void Render(string title, string content);
@@ -21,33 +31,37 @@ namespace BehavioralPattern02.BridgePattern
         }
     }
 
-    public class Invoice
+    public class Invoice : Documentt
     {
         private readonly IDocRenderer _docRenderer;
-        public Invoice(IDocRenderer docRenderer)
+
+        public Invoice(IDocRenderer renderer) : base(renderer)
         {
-            _docRenderer = docRenderer;
         }
+
         public string Title {get; set;}  ="Invoice";
         public string Content {get; set;}  ="Invoice details ...";
 
-        public void Render()
+       
+
+        public override void Render()
         {
             _docRenderer.Render(Title, Content);
         }
     }
 
-    public class Report
+    public class Report : Documentt
     {
         public IDocRenderer _docRenderer;
-        public Report(IDocRenderer docRenderer)
+
+        public Report(IDocRenderer renderer) : base(renderer)
         {
-            _docRenderer = docRenderer;
         }
-         public string Title { get; set; } = "Report";
+
+        public string Title { get; set; } = "Report";
         public string Content { get; set; } = "Monthly stats...";
 
-        public void Render()
+        public override void Render()
         {
             _docRenderer.Render(Title, Content);
         }
