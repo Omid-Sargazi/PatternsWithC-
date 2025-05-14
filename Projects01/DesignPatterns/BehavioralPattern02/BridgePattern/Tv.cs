e57668ad0465c51f56f32aba5bc36516e7a28e05
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-14 18:38:30 +0330

    Introduced IDevice interface and updated Remote to support multiple devices

--- Projects01/DesignPatterns/BehavioralPattern02/BridgePattern/Tv.cs
+++ Projects01/DesignPatterns/BehavioralPattern02/BridgePattern/Tv.cs
@@ -1,23 +1,35 @@
 namespace BehavioralPattern02.BridgePattern
 {
-    public class Tv
+    public interface IDevice
+    {
+        void TurnOn();
+        void TurnOff();
+    }
+    public class Tv : IDevice
     {
         public void TurnOn()=>Console.WriteLine("TV is ON");
         public void TurnOff() => Console.WriteLine("TV is Off");
     }
 
+    public class Radio : IDevice
+    {
+         public void TurnOn() => Console.WriteLine("Radio is ON");
+
+       public void TurnOff() => Console.WriteLine("Radio is OFF");
+    }
+
     public class Remote
     {
-        private Tv _tv;
-        public Remote(Tv tv)
+        private IDevice _device;
+        public Remote(IDevice device)
         {
-            _tv = tv;
+            _device = device;
         }
-
+        
         public void TogglePower()
         {
              Console.WriteLine("Using Remote to toggle power.");
-            _tv.TurnOn();
+           _device.TurnOn();
         }
     }
 }
