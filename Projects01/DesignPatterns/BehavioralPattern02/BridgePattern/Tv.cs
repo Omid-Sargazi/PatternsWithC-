5ebede0e68f80a71d7b6ef04496e9ee7c48535c8
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-14 18:45:37 +0330

    Applied Bridge Pattern: decoupled Remote abstraction from Device implementation

--- Projects01/DesignPatterns/BehavioralPattern02/BridgePattern/Tv.cs
+++ Projects01/DesignPatterns/BehavioralPattern02/BridgePattern/Tv.cs
@@ -4,11 +4,17 @@ namespace BehavioralPattern02.BridgePattern
     {
         void TurnOn();
         void TurnOff();
+        void SetVolume(int percent);
+        void Mute();
     }
     public class Tv : IDevice
     {
         public void TurnOn()=>Console.WriteLine("TV is ON");
         public void TurnOff() => Console.WriteLine("TV is Off");
+
+        public void SetVolume(int percent) => Console.WriteLine($"TV volume set to {percent}%");
+
+       public void Mute() => Console.WriteLine("TV muted");
     }
 
     public class Radio : IDevice
@@ -16,11 +22,15 @@ namespace BehavioralPattern02.BridgePattern
          public void TurnOn() => Console.WriteLine("Radio is ON");
 
        public void TurnOff() => Console.WriteLine("Radio is OFF");
+
+        public void Mute() => Console.WriteLine("Radio muted");
+
+        public void SetVolume(int percent) => Console.WriteLine($"Radio volume set to {percent}%");
     }
 
     public class Remote
     {
-        private IDevice _device;
+        protected IDevice _device;
         public Remote(IDevice device)
         {
             _device = device;
@@ -30,6 +40,25 @@ namespace BehavioralPattern02.BridgePattern
         {
              Console.WriteLine("Using Remote to toggle power.");
            _device.TurnOn();
+        }
+    }
+
+    public class AdvancedRemote : Remote
+    {
+        public AdvancedRemote(IDevice device) : base(device)
+        {
+        }
+
+        public void Mute()
+        {
+            Console.WriteLine("Advanced remote muting device...");
+            _device.Mute();
+        }
+
+        public void SetVolume(int percent)
+        {
+            Console.WriteLine($"Advanced remote setting volume to {percent}%...");
+             _device.SetVolume(percent);
         }
     }
 }
