78fcfe7e9429cbf79a5454fba33eca7662c3fc76
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-14 22:08:08 +0330

    Added hardcoded subclasses for coffee variants (milk, sugar, milk+ sugar)

--- Projects01/DesignPatterns/BehavioralPattern02/DecoratorPattern/Coffee.cs
+++ Projects01/DesignPatterns/BehavioralPattern02/DecoratorPattern/Coffee.cs
@@ -5,4 +5,38 @@ namespace BehavioralPattern02.DecoratorPattern
         public virtual string GetDescription() => "Plain Coffee";
         public virtual double GetCost() => 2.0;
     }
+
+    public class CoffeeWithMilk : Coffee
+    {
+        public override string GetDescription()
+        {
+            return "Coffee with Milk";
+        }
+
+        public override double GetCost()
+        {
+            return 2.5;
+        }
+    }
+
+    public class CoffeeWithSugar  :Coffee
+    {
+        public override string GetDescription()
+        {
+            return "Coffee with Sugar";
+        }
+        public override double GetCost()=>2.3;
+    }
+
+    public class CoffeeWithMilkAndSugar : Coffee
+    {
+        public override string GetDescription()
+        {
+            return "Coffee with Milk and Sugar";
+        }
+        public override double GetCost()
+        {
+            return 2.8;
+        }
+    }
 }
