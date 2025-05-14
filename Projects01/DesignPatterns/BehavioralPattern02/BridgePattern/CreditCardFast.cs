73440489fe078098abc691b3b1cc746932dbeb3f
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-14 19:44:39 +0330

    combine two design patterns,startegy and bridge

--- Projects01/DesignPatterns/BehavioralPattern02/BridgePattern/CreditCardFast.cs
+++ Projects01/DesignPatterns/BehavioralPattern02/BridgePattern/CreditCardFast.cs
@@ -1,12 +1,53 @@
 namespace BehavioralPattern02.BridgePattern
 {
-    public class CreditCardFast
+    public interface IProcessingStrategy
     {
-        public void ProcessPayment(decimal amount)
+        void Process(decimal amount);
+    }
+
+    public interface IPaymentMethod
+    {
+        void ExecutePayment(decimal amount);
+    }
+
+    public class FastProcessing : IProcessingStrategy
+    {
+        public void Process(decimal amount)
         {
-            Console.WriteLine($"Processing {amount:C} with Credit Card (Fast mode)");
+            Console.WriteLine($"Processing {amount:C} in Fast mode");
         }
     }
+
+    public class SecureProcessing : IProcessingStrategy
+    {
+        public void Process(decimal amount)
+        {
+            Console.WriteLine($"Processing {amount:C} in Secure mode");
+        }
+    }
+
+
+    public class CreditCardMethod : IPaymentMethod
+    {
+        public void ExecutePayment(decimal amount)
+        {
+            
+            Console.WriteLine($"Processing {amount:C} with Credit Card (Fast mode)");
+        }
+
+       
+    }
+
+    public class PayPalMethod : IPaymentMethod
+    {
+        public void ExecutePayment(decimal amount)
+        {
+            Console.WriteLine($"Executing payment of {amount:C} via PayPal");
+        }
+    }
+
+    
+
 
     public class CreditCardSecure
     {
@@ -29,6 +70,32 @@ namespace BehavioralPattern02.BridgePattern
         public void ProcessPayment(decimal amount)
         {
             Console.WriteLine($"Processing {amount:C} with PayPal (Secure mode)");
+        }
+    }
+
+    public abstract class Payment
+    {
+        protected IPaymentMethod _paymentMethod;
+        protected IProcessingStrategy _processingStrategy;
+        public Payment(IPaymentMethod paymentMethod, IProcessingStrategy processingStrategy)
+        {
+            _paymentMethod = paymentMethod;
+            _processingStrategy = processingStrategy;
+        }
+
+        public abstract void ProcessPayment(decimal amount);
+    }
+
+    public class StandardPayment : Payment
+    {
+        public StandardPayment(IPaymentMethod paymentMethod, IProcessingStrategy processingStrategy) : base(paymentMethod, processingStrategy)
+        {
+        }
+
+        public override void ProcessPayment(decimal amount)
+        {
+           _processingStrategy.Process(amount);
+           _paymentMethod.ExecutePayment(amount);
         }
     }
 }
