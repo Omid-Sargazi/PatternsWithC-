d90393cc5d54d259e36b72edb908f3786b3b891f
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-15 21:59:52 +0330

    Mediator Pattern for chat room project

--- Projects01/DesignPatterns/DesignPatterns/Mediator/User.cs
+++ Projects01/DesignPatterns/DesignPatterns/Mediator/User.cs
@@ -1,27 +1,55 @@
 namespace DesignPatterns.Mediator
 {
+    public interface IChatRoom
+    {
+        void RegisterUser(User user);
+        void SendMessage(string message, User sender);
+    }
+
+    public class ChatRoom : IChatRoom
+    {
+        private List<User> _users = new List<User>();
+
+        public void RegisterUser(User user)
+        {
+            _users.Add(user);
+            Console.WriteLine($"{user.Name} joined the chat room.");
+        }
+
+        public void SendMessage(string message, User sender)
+        {
+            Console.WriteLine($"{sender.Name} sends: {message}");
+            foreach(var user in _users)
+            {
+                if(user != sender)
+                {
+                    user.ReceiveMessage(message, sender.Name);
+                }
+            }
+        }
+    }
+
     public class User
     {
+        private IChatRoom _chatRoom;
         private string _name;
-        private List<User> _users;
+        public string Name =>_name;
+
+        public void SetChatRoom(IChatRoom chatRoom)
+        {
+            _chatRoom = chatRoom;
+        }
+        
         public User(string name)
         {
             _name = name;
-            _users = new List<User>();
         }
 
-        public void AddContact(User user)
-        {
-            _users.Add(user);
-        }
+        
 
         public void SendMessage(string message)
         {
-            Console.WriteLine($"{_name} sends: {message}");
-            foreach(var user in _users)
-            {
-                user.ReceiveMessage(message,_name);
-            }
+           _chatRoom.SendMessage(message,this);
         }
         public void ReceiveMessage(string message, string sender)
         {
