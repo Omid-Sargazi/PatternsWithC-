f5e6283c645ad345766044970abf3bc6268247bb
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-18 18:39:46 +0330

    define a mediator pattern for chatroom

--- Projects01/DesignPatterns/DesignPatterns/MediatorPattern/User.cs
+++ Projects01/DesignPatterns/DesignPatterns/MediatorPattern/User.cs
@@ -5,30 +5,62 @@ namespace DesignPatterns.MediatorPattern
         void RegisterUser(User user);
         void SendMessage(string message, User user);
     }
+
+    public class ChatRoom : IChatRoom
+    {
+        private List<User> _users;
+        public ChatRoom(User user)
+        {
+            _users = new List<User>();
+        }
+        public void RegisterUser(User user)
+        {
+            _users.Add(user);
+            user.SetChatRoom(this);
+            Console.WriteLine($"{user._name} joined the chat room.");
+        }
+
+        public void SendMessage(string message, User sender)
+        {
+            Console.WriteLine($"{sender._name} sends: {message}");
+            foreach (var user in _users)
+            {
+                if (user != sender)
+                {
+                    user.ReceiveMessage(message, sender._name);
+                }
+            }
+        }
+    }
+
     public class User
     {
-        private string _name;
-        private List<User> _users;
+        public string _name;
         public User(string name)
         {
             _name = name;
         }
 
-        public void AddContact(User user)
+        private IChatRoom _chatRoom;
+        public User(IChatRoom chatRoom)
         {
-            _users.Add(user);
+            _chatRoom = chatRoom;
         }
+
+        public void SetChatRoom(IChatRoom chatRoom)
+        {
+            _chatRoom = chatRoom;
+        }
+       
+
+       
 
         public void SendMessage(string message)
         {
-            Console.WriteLine($"{_name} sends: {message}");
-            foreach (var user in _users)
-            {
-                user.ReceiveMessage(message, _name);
-            }
+            _chatRoom.SendMessage(message,this);
         }
 
-        private void ReceiveMessage(string message, string sender)
+        public void ReceiveMessage(string message, string sender)
         {
             Console.WriteLine($"{_name} received from {sender}:{message}");
         }
