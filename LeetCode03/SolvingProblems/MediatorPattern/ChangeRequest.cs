ceaf5d43d8c84c0126ec7e2e6ad55ce7aa631edf
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-19 18:19:56 +0330

    define and impliy client and employee in manage class

--- LeetCode03/SolvingProblems/MediatorPattern/ChangeRequest.cs
+++ LeetCode03/SolvingProblems/MediatorPattern/ChangeRequest.cs
@@ -2,15 +2,44 @@ namespace SolvingProblems.MediatorPattern
 {
     public interface IProjectManager
     {
-        void RegisterEmployee();
-        void RegisterClient();
-        void SendChangeRequest();
-        void SendProgressRepost();
+        void RegisterEmployee(Employee employee);
+        void RegisterClient(Client client);
+        void SendChangeRequest(ChangeRequest changeRequest, Client client);
+        void SendProgressRepost(ProgressReport progressReport, Employee employee);
     }
     public interface IColleague
     {
         void ReceiveNotification(string message);
     }
+
+    public class ProjectManager : IProjectManager
+    {
+        private readonly List<Employee> _employees = new List<Employee>();
+        private readonly List<Client> _clients = new List<Client>();
+        private readonly Dictionary<int, Client> _requestToClient = new Dictionary<int, Client>();
+
+        public void RegisterClient(Client client)
+        {
+            _clients.Add(client);
+            Console.WriteLine($"Client {client.Name} registered.");
+        }
+
+        public void RegisterEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendChangeRequest(ChangeRequest changeRequest, Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendProgressRepost(ProgressReport progressReport, Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     public class ChangeRequest
     {
         public int Id { get; set; }
@@ -30,6 +59,40 @@ namespace SolvingProblems.MediatorPattern
         {
             TaskId = taskId;
             Status = status;
+        }
+    }
+
+    public class Employee : IColleague
+    {
+        private readonly IProjectManager _projectManager;
+        public Employee(IProjectManager projectManager)
+        {
+            _projectManager = projectManager;
+        }
+        public void SetProjectManager()
+        {
+            _projectManager.RegisterEmployee(this);
+        }
+        public void ReceiveNotification(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Client : IColleague
+    {
+        private readonly IProjectManager _projectManager1;
+        public Client(IProjectManager projectManager)
+        {
+            _projectManager1 = projectManager;
+        }
+        public void SetProjectManager()
+        {
+            _projectManager1.RegisterClient(this);
+        }
+        public void ReceiveNotification(string message)
+        {
+            throw new NotImplementedException();
         }
     }
 }
