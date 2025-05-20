dce0179b27ecd779391ed00053f84f1383827331
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-20 23:05:56 +0330

    complete book controller and add DTOs

--- SQLServerProjects/SQLServerTest/Controllers/BooksController.cs
+++ SQLServerProjects/SQLServerTest/Controllers/BooksController.cs
@@ -17,35 +17,54 @@ namespace SQLServerTest.Controllers
 
         // GET: api/Books
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+        public async Task<ActionResult<IEnumerable<BookDto>>> GetBooks()
         {
-            return await _context.Books.Include(b => b.Author).ToListAsync();
+            return await _context.Books.Select(b => new BookDto
+            {
+                Id = b.Id,
+                Title = b.Title,
+                AuthorId = b.AuthorId
+            }).ToListAsync();
         }
 
         // GET: api/Books/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Book>> GetBook(int id)
+        public async Task<ActionResult<BookDto>> GetBook(int id)
         {
-            var book = await _context.Books.Include(b => b.Author).FirstOrDefaultAsync(b => b.Id == id);
+            var book = await _context.Books
+           .Select(b => new BookDto
+           {
+               Id = b.Id,
+               Title = b.Title,
+               AuthorId = b.AuthorId,
+           }).FirstOrDefaultAsync(b => b.Id == id);
             if (book == null) return NotFound();
             return book;
         }
 
         // POST: api/Books
         [HttpPost]
-        public async Task<ActionResult<Book>> CreateBook(Book book)
+        public async Task<ActionResult<BookDto>> CreateBook(BookDto bookDto)
         {
+            var book = new Book
+            {
+                Title = bookDto.Title,
+                AuthorId = bookDto.AuthorId
+            };
             _context.Books.Add(book);
             await _context.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetBook), new { id = book.Id }, book);
+            bookDto.Id = book.Id;
+            return CreatedAtAction(nameof(GetBook), new { Id = bookDto.Id }, bookDto);
         }
 
         // PUT: api/Books/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateBook(int id, Book book)
+        public async Task<IActionResult> UpdateBook(int id, BookDto bookDto)
         {
-            if (id != book.Id) return BadRequest();
-            _context.Entry(book).State = EntityState.Modified;
+            if (id != bookDto.Id) return BadRequest();
+            var book = await _context.Books.FindAsync(id);
+            book.Title = bookDto.Title;
+            book.AuthorId = bookDto.AuthorId;
             try
             {
                 await _context.SaveChangesAsync();
@@ -55,6 +74,7 @@ namespace SQLServerTest.Controllers
                 if (!_context.Books.Any(b => b.Id == id)) return NotFound();
                 throw;
             }
+
             return NoContent();
         }
 
