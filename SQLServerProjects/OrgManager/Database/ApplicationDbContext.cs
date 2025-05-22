e107cfcf5937768db97652e2fa1be0292686408b
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-23 00:16:14 +0330

    define all dbsets for all entities

--- SQLServerProjects/OrgManager/Database/ApplicationDbContext.cs
+++ SQLServerProjects/OrgManager/Database/ApplicationDbContext.cs
@@ -20,6 +20,43 @@ namespace OrgManager.Database
             .WithMany(e => e.Subordinates)
             .HasForeignKey(e => e.ManagerId)
             .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Employee>()
+            .HasOne(e => e.Department)
+            .WithMany(d => d.Employees)
+            .HasForeignKey(e => e.DepartmentId);
+
+            modelBuilder.Entity<Department>()
+            .HasOne(d => d.ParentDepartment)
+            .WithMany(d => d.SubDepartments)
+            .HasForeignKey(d => d.ParentDepartmentId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<TaskItem>()
+            .HasOne(t => t.parentTask)
+            .WithMany(t => t.SubTasks)
+            .HasForeignKey(t => t.ParentTaskId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<TaskItem>()
+            .HasOne(t => t.Employee)
+            .WithMany(e => e.Tasks)
+            .HasForeignKey(t => t.EmployeeId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<TaskItem>().
+            HasOne(t => t.Category)
+            .WithMany(c => c.Tasks)
+            .HasForeignKey(t => t.CategoryId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            
+            modelBuilder.Entity<Category>()
+            .HasOne(c => c.ParentCategory)
+            .WithMany(c => c.SubCategories)
+            .HasForeignKey(c => c.ParentCategoryId)
+            .OnDelete(DeleteBehavior.Restrict);
+            
         }
     }
 }
