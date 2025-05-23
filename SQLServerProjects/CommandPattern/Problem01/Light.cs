3d4a42aab15da0d65ef14f3e340767bbab597521
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-23 15:21:15 +0330

    improve the light controller by command pattern

--- SQLServerProjects/CommandPattern/Problem01/Light.cs
+++ SQLServerProjects/CommandPattern/Problem01/Light.cs
@@ -11,24 +11,60 @@ namespace CommandPattern.Problem01
         public void TurnOff() => Console.WriteLine($"Light is Off");
     }
 
-    public class RemoteControll
+    public class LightOnCommand : ICommandControllerLight
     {
         private Light _light;
-        public RemoteControll(Light light)
+        public LightOnCommand(Light light)
         {
             _light = light;
         }
 
+        public void Execute()
+        {
+            _light.TurnOn();
+        }
+
+        public void Undo()
+        {
+            _light.TurnOff();
+        }
+    }
+
+    public class LightOffCommand : ICommandControllerLight
+    {
+        private Light _light;
+        public LightOffCommand(Light light)
+        {
+            _light = light;
+        }
+
+        public void Execute()
+        {
+            _light.TurnOff();
+        }
+
+        public void Undo()
+        {
+            _light.TurnOn();
+        }
+    }
+
+    public class RemoteControll
+    {
+        private ICommandControllerLight _command;
+        private Light _light;
+        public RemoteControll(ICommandControllerLight command)
+        {
+            _command = command;
+        }
+
         public void PressButton(string action)
         {
-            if (action == "ON")
-            {
-                _light.TurnOn();
-            }
-            else if (action == "OFF")
-            {
-                _light.TurnOff();
-            }
+            _command.Execute();
+        }
+        public void PressUndo()
+        {
+            _command.Undo();
         }
     }
 }
