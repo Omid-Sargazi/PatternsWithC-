4ac3b311799cda642a642dbbc967b48f2b10d63e
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-25 20:46:36 +0330

    configure all entities in AppDbContext

--- SQLServerProjects/ServicesInProjects/Data/AppDbContext.cs
+++ SQLServerProjects/ServicesInProjects/Data/AppDbContext.cs
@@ -6,6 +6,21 @@ namespace ServicesInProjects.Data
     public class AppDbContext : DbContext
     {
         public DbSet<TodoItem> TodoItems { get; set; }
-        public AppDbContext(DbContextOptions<AppDbContext> options):base(options){}
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Tasks> Taskss { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tasks>()
+            .HasOne(t => t.Project)
+            .WithMany(p => p.Taskss)
+            .HasForeignKey(t => t.ProjectId);
+
+            modelBuilder.Entity<Note>()
+            .HasOne(n => n.Project)
+            .WithMany(p => p.Notes)
+            .HasForeignKey(n => n.ProjectId);
+        }
     }
 }
