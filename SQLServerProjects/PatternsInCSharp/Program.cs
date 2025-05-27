60f7657b46670895216d793aad8680767479f3f7
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-27 22:35:19 +0330

    Add Program class to test game event system - Implemented Main method to simulate player actions (move, send message, collect item) via mediator.

--- SQLServerProjects/PatternsInCSharp/Program.cs
+++ SQLServerProjects/PatternsInCSharp/Program.cs
@@ -13,10 +13,19 @@ Console.WriteLine("Hello, World!");
 // omid.send("Hello");
 // Zini.send("hi");
 
-Kitchen kitchen = new Kitchen();
-Cashier cashier = new Cashier();
-Waiter waiter = new Waiter();
+// Kitchen kitchen = new Kitchen();
+// Cashier cashier = new Cashier();
+// Waiter waiter = new Waiter();
 
-IRestaurantMediator restaurantMediator = new OrderManagementSystem(kitchen, cashier, waiter);
-Customer customer = new Customer("Omid", restaurantMediator);
-customer.PlaceOrder("Pizza");
+// IRestaurantMediator restaurantMediator = new OrderManagementSystem(kitchen, cashier, waiter);
+// Customer customer = new Customer("Omid", restaurantMediator);
+// customer.PlaceOrder("Pizza");
+
+GameServer gameServer = new GameServer();
+ChatSystem chatSystem = new ChatSystem();
+
+IGameMediator gameMediator = new GameEventSystem(chatSystem, gameServer);
+Player player = new Player("omid", gameMediator);
+player.Move("(10,10)");
+player.SendMassage("hi to all");
+player.CollectItem("golden swor");
