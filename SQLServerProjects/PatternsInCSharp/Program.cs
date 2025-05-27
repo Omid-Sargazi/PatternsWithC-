b6f1a2519295fa68c93a1c3df799810a403e3564
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-27 20:06:19 +0330

    Test mediator pattern with two user

--- SQLServerProjects/PatternsInCSharp/Program.cs
+++ SQLServerProjects/PatternsInCSharp/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using PatternsInCSharp.MediatorPattern;
+
 Console.WriteLine("Hello, World!");
+IMediator mediator = new Mediator();
+
+User omid = new User("Omid", mediator);
+User Zini = new User("Zini", mediator);
+mediator.AddUser(omid);
+mediator.AddUser(Zini);
+mediator.SendMessage("Hello", omid);
+omid.send("Hello");
+Zini.send("hi");
