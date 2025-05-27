5438862395589321efd6ecccd0686f542feaea57
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-27 22:05:08 +0330

    usage of the project

--- SQLServerProjects/PatternsInCSharp/Program.cs
+++ SQLServerProjects/PatternsInCSharp/Program.cs
@@ -1,13 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime;
 using PatternsInCSharp.MediatorPattern;
 
 Console.WriteLine("Hello, World!");
-IMediator mediator = new Mediator();
+// IMediator mediator = new Mediator();
 
-User omid = new User("Omid", mediator);
-User Zini = new User("Zini", mediator);
-mediator.AddUser(omid);
-mediator.AddUser(Zini);
-mediator.SendMessage("Hello", omid);
-omid.send("Hello");
-Zini.send("hi");
+// User omid = new User("Omid", mediator);
+// User Zini = new User("Zini", mediator);
+// mediator.AddUser(omid);
+// mediator.AddUser(Zini);
+// mediator.SendMessage("Hello", omid);
+// omid.send("Hello");
+// Zini.send("hi");
+
+Kitchen kitchen = new Kitchen();
+Cashier cashier = new Cashier();
+Waiter waiter = new Waiter();
+
+IRestaurantMediator restaurantMediator = new OrderManagementSystem(kitchen, cashier, waiter);
+Customer customer = new Customer("Omid", restaurantMediator);
+customer.PlaceOrder("Pizza");
