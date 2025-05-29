f662aed635a56597ca52e865b8c0505e488fa4f8
Omid-Sargazi <om.sargazi@gmail.com>
2025-05-29 23:09:43 +0330

    5 top products with include query

--- SQLServerProjects/AdventureWorksLT2019/query.sql
+++ SQLServerProjects/AdventureWorksLT2019/query.sql
@@ -1,17 +1,5 @@
-USE AdventureWorks2019;
-GO
-SELECT TOP 10 p.FirstName, p.LastName
-FROM Person.Person p
-JOIN Sales.Customer c ON p.BusinessEntityID = c.PersonID
-WHERE p.PersonType IN ('IN', 'SC');
+USE AdventureWorksLT2019;
 GO
 
-
-SELECT TOP 5 BusinessEntityID, FirstName, LastName, PersonType
-FROM Person.Person WHERE PersonType IN ('IN', 'SC');
-GO
-
-SELECT TOP 5 CustomerID, PersonID
-FROM Sales.Customer
-WHERE PersonID IS NOT NULL;
+EXEC sp_fkeys @pktable_name = 'SalesOrderHeader', @pktable_owner = 'SalesLT';
 GO
