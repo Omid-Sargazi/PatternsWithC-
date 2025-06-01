bb21b587d6aae6ef7a27d23d2703226b41203edb
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-01 19:37:29 +0330

    Product model

--- SQLServerProjects/AdventureWorksLT2019-2/Models/Customer..cs
+++ SQLServerProjects/AdventureWorksLT2019-2/Models/Customer..cs
@@ -13,4 +13,19 @@ namespace AdventureWorksLT2019_2.Models
         public string? EmailAddress { get; set; }
         public string? CompanyName { get; set; }
     }
+
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        public string? Name { get; set; }
+        public string? ProductNumber { get; set; }
+        public decimal ListPrice { get; set; }
+        public int? ProductCategoryID { get; set; }
+        public int? ProductModelID { get; set; }
+        [ForeignKey("ProductCategoryID")]
+        public ProductCategory? ProductCategory { get; set; }
+        [ForeignKey("ProductModelID")]
+        public ProductModel? ProductModel { get; set; }
+    }
 }
