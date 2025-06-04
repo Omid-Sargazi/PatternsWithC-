66907d159f9ac3e313475a09ba1c66ff32753fba
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-04 11:36:12 +0330

    Tading App for notifying all obsever

---
+++ SQLServerProjects/AdventureWorksConsole/ObserverPattern/IObserverStock.cs
@@ -0,0 +1,55 @@
+namespace AdventureWorksConsole.ObserverPattern;
+
+public interface IObserverStock
+{
+    void Update(string stockSymbol, decimal price);
+}
+
+public interface ISubjectStock
+{
+    void RegisterObserver(IObserverStock observer);
+    void RemoveObserver(IObserverStock observer);
+    void NotifyObservers();
+}
+
+public class StockMarket : ISubjectStock
+{
+    private List<IObserverStock> _observers = new List<IObserverStock>();
+    private decimal _price;
+    private string _stockSymbol;
+    public StockMarket(string stockSymbol)
+    {
+        _stockSymbol = stockSymbol;
+    }
+    public void SetPrice(decimal price)
+    {
+        _price = price;
+        NotifyObservers();
+    }
+    public void NotifyObservers()
+    {
+        foreach (var observer in _observers)
+        {
+            observer.Update(_stockSymbol, _price);
+        }
+    }
+
+    public void RegisterObserver(IObserverStock observer)
+    {
+        _observers.Add(observer);
+    }
+
+    public void RemoveObserver(IObserverStock observer)
+    {
+        _observers.Remove(observer);
+    }
+}
+
+
+public class TradingApp : IObserver
+{
+    public void Update(float temp)
+    {
+        throw new NotImplementedException();
+    }
+}
--- SQLServerProjects/AdventureWorksConsole/obj/Debug/net9.0/AdventureWorksConsole.AssemblyInfo.cs
+++ SQLServerProjects/AdventureWorksConsole/obj/Debug/net9.0/AdventureWorksConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventureWorksConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76119226aab27704cb9375418ae453b25118f058")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+949de310c39eefc8a02a7302bd51a7a99f891d7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventureWorksConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventureWorksConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
