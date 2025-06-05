c21657c1ce2fbe608a508cb88936b8d31539520d
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-05 19:17:52 +0330

    define some models for getting detail orders

--- SQLServerProjects/AdventureWorksDatabase/Models/Models.cs
+++ SQLServerProjects/AdventureWorksDatabase/Models/Models.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.Identity.Client;
 
 namespace AdventureWorksDatabase.Models
 {
@@ -13,7 +14,7 @@ namespace AdventureWorksDatabase.Models
         public DateTime ReviewDate { get; set; }
         public string EmailAddress { get; set; }
         public string Comments { get; set; }
-        
+
     }
 
     [Table("Product", Schema = "Production")]
@@ -24,4 +25,45 @@ namespace AdventureWorksDatabase.Models
         public string Name { get; set; }
 
     }
+
+    [Table("Customer", Schema = "Sales")]
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        public int PersonId { get; set; }
+
+    }
+    [Table("Person", Schema = "Person")]
+    public class Person
+    {
+        [Key]
+        public int BusinessEntityID { get; set; }
+        public string FirstName { get; set; }
+        public int LastName { get; set; }
+    }
+
+    [Table("SalesOrderHeader", Schema = "Sales")]
+    public class SalesOrderHeader
+    {
+        [Key]
+        public int SalesOrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int SalesOrderNumber { get; set; }
+        public int CUstomerId { get; set; }
+        public int SalesPersonID { get; set; }
+    }
+
+    [Table("SalesOrderDetail", Schema = "Sales")]
+    public class SalesOrderDetail
+    {
+        [Key]
+        public int SalesOrderID { get; set; }
+        public int ProductID { get; set; }
+        public int OrderQty { get; set; }
+
+        public decimal LineTotal { get; set; }
+    }
+
+
 }
