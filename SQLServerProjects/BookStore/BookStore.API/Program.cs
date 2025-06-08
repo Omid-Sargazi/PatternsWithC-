4e95e0926ec43f7b577699f5369732c0c0ef99eb
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-08 21:34:04 +0330

    add connection string

--- SQLServerProjects/BookStore/BookStore.API/Program.cs
+++ SQLServerProjects/BookStore/BookStore.API/Program.cs
@@ -1,15 +1,21 @@
 using BookStore.Application.Services;
 using BookStore.Domain.Interfaces;
+using BookStore.Infrastructure.Data;
 using BookStore.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<BookDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<IBookRepository, InMemoryBookRepository>();
+builder.Services.AddScoped<IBookRepository, EFBookRepository>();
 builder.Services.AddScoped<BookService>();
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
