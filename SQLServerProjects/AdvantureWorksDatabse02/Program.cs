80153960eda4840765178508cf7aabe120f4893e
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-11 19:24:47 +0330

    .

--- SQLServerProjects/AdvantureWorksDatabse02/Program.cs
+++ SQLServerProjects/AdvantureWorksDatabse02/Program.cs
@@ -3,21 +3,34 @@
     
     // See https://aka.ms/new-console-template for more information
 public delegate int Calculator(int a, int b);
-public class Program
-{
-    public static  void Main(string[] args)
+    public class Program
     {
-        Console.WriteLine("Hello, World!");
-        Calculator calc = Add;
-        int result = calc(5, 3);
-        Console.WriteLine(result);
-    }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
 
-    public static int Add(int x, int y)
-    {
-        int sum = x + y;
-        Console.WriteLine($"Add:{sum}");
-        return sum;
+            Calculator calc = Add;
+            int result = calc(5, 3);
+            Console.WriteLine(result);
+
+            calc = Multiple;
+            result = calc(5, 3);
+            Console.WriteLine(result);
+            
+        }
+
+        public static int Add(int x, int y)
+        {
+            int sum = x + y;
+            Console.WriteLine($"Add:{sum}");
+            return sum;
+        }
+
+        public static int Multiple(int a, int b)
+        {
+            int mul = a * b;
+            Console.WriteLine($"Multiple is:{mul}");
+            return mul;
+        }
     }
-}
 }
--- SQLServerProjects/AdvantureWorksDatabse02/obj/Debug/net9.0/AdvantureWorksDatabse02.AssemblyInfo.cs
+++ SQLServerProjects/AdvantureWorksDatabse02/obj/Debug/net9.0/AdvantureWorksDatabse02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvantureWorksDatabse02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2879d3dbc1386e4773d8b4400eea88eda7987ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beec7210c1606ef7c2eb89988d7b087ff59e1da0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvantureWorksDatabse02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvantureWorksDatabse02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
