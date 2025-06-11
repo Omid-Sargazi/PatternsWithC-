31fc1f84064afcb34b6b708b0c4d0b3900828e38
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-12 00:05:41 +0330

    .

--- SQLServerProjects/AdvantureWorksDatabse02/ExtensionMethod/StringExtensions.cs
+++ SQLServerProjects/AdvantureWorksDatabse02/ExtensionMethod/StringExtensions.cs
@@ -42,17 +42,33 @@ namespace AdvantureWorksDatabse02.ExtensionMethod
 
             return $"{year}/{month:00}/{day:00}";
         }
-            
-            public static bool IsWeekend(this DateTime date)
+
+        public static bool IsWeekend(this DateTime date)
         {
-            return date.DayOfWeek == DayOfWeek.Friday || 
+            return date.DayOfWeek == DayOfWeek.Friday ||
                 date.DayOfWeek == DayOfWeek.Saturday;
         }
-        
+
         public static DateTime StartOfWeek(this DateTime date)
         {
             int diff = (7 + (date.DayOfWeek - DayOfWeek.Saturday)) % 7;
             return date.AddDays(-1 * diff).Date;
+        }
+    }
+
+    public static class NumericExtensions
+    {
+        public static bool IsPrime(this int number)
+        {
+            if (number < 2) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
         }
     }
 
