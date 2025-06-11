052c7a9ef7e0bb726c616f3f57951315fd76e63e
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-11 21:31:58 +0330

    usage of delegate

--- SQLServerProjects/AdvantureWorksDatabse02/StrategyPattern/ChocolateFactorySimple.cs
+++ SQLServerProjects/AdvantureWorksDatabse02/StrategyPattern/ChocolateFactorySimple.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualBasic;
+
 namespace AdvantureWorksDatabse02.StrategyPattern
 {
     public class ChocolateFactorySimple
@@ -12,6 +14,48 @@ namespace AdvantureWorksDatabse02.StrategyPattern
                 if (chocolate.Contains("A"))
                     AChocolate.Add(chocolate);
             }
+        }
+    }
+
+    public class ChocolateFactoryMessy
+    {
+        public List<string> ProcessChocolates(List<string> chocolates, string operation)
+        {
+            if (operation == "only-A")
+            {
+                return chocolates.Where(c => c.Contains("A")).ToList();
+            }
+
+            else if (operation == "only-B")
+                return chocolates.Where(c => c.Contains("B")).ToList();
+
+            else if (operation.Contains("make-uppercase"))
+                return chocolates.Select(c => c.ToUpper()).ToList();
+
+            return chocolates;
+        }
+
+        public class ChocolateFactorySmart
+        {
+            public delegate IEnumerable<string> ChocolateProcessor(IEnumerable<string> chocolates);
+            public void ChocolateFactory()
+            {
+                var chocolates = new List<string> { "تلخ", "شیری", "سفید", "کاکائویی", "نارگیلی" };
+
+                ChocolateProcessor filterMilky = chocs => chocs.Where(c => c.Contains(""));
+
+                ChocolateProcessor addPremium = chocs => chocs.Select(c => "premium" + c);
+
+                ChocolateProcessor makeUpper = chocs => chocs.Select(c => c.ToUpper());
+
+                var result01 = ProcessChocolate(chocolates, addPremium);
+            }
+
+            public static IEnumerable<string> ProcessChocolate(IEnumerable<string> chocolates, ChocolateProcessor
+             processor)
+            {
+                return processor(chocolates);
+             }
         }
     }
 }
