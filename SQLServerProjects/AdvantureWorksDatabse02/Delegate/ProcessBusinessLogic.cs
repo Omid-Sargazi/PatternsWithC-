6d033ded484ef404c494afaea6cae53fae059384
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-12 11:20:08 +0330

    calculator with delegate

--- SQLServerProjects/AdvantureWorksDatabse02/Delegate/ProcessBusinessLogic.cs
+++ SQLServerProjects/AdvantureWorksDatabse02/Delegate/ProcessBusinessLogic.cs
@@ -1,3 +1,6 @@
+using Azure;
+using Azure.Core.Pipeline;
+
 namespace AdvantureWorksDatabse02.Delegate
 {
     public delegate void Notify();
@@ -15,8 +18,57 @@ namespace AdvantureWorksDatabse02.Delegate
         }
 
         static void ShowMessage()
-    {
-        Console.WriteLine("Process Completed Successfully!");
+        {
+            Console.WriteLine("Process Completed Successfully!");
+        }
     }
+
+
+    public class Calculator
+    {
+        public delegate int Calc(int x, int y);
+        private Dictionary<string, Calc> dicCalc;
+        public Calculator()
+        {
+            dicCalc = new Dictionary<string, Calc>
+            {
+                ["add"] = Add,
+                ["multiply"] = Multilple,
+                ["subtract"] = Subtract,
+                ["divid"] = Divide,
+            };
+        }
+
+
+
+        public void calculator(int a, int b, string operation)
+        {
+            if (dicCalc.ContainsKey(operation))
+            {
+                dicCalc[operation](a, b);
+            }
+            else Console.WriteLine("This operation is not valid");
+        }
+
+
+        private int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        private int Multilple(int x, int y)
+        {
+            return x * y;
+        }
+
+        private int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        private int Subtract(int a, int b)
+        {
+            return a - b;
+        }
     }
 }
