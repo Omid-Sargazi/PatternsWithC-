a0df503a7172c80b2c719801f424f8ebc7937e28
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-13 00:30:12 +0330

    .

--- SQLServerProjects/AdventureWorksDatabase/Program.cs
+++ SQLServerProjects/AdventureWorksDatabase/Program.cs
@@ -4,18 +4,4 @@ using AdventureWorksDatabase.Models;
 using Microsoft.EntityFrameworkCore;
 
 Console.WriteLine("Hello, World!");
-var context = new AppDbContext();
 
-// نادرست:
-var task1 = context.Products.ToListAsync();
-var task2 = context.Customers.ToListAsync();
-await Task.WhenAll(task1, task2);
-
-// صحیح:
-var products = await context.Products.ToListAsync();
-var customers = await context.Customers.ToListAsync();
-
-foreach (var p in products)
-{
-    Console.WriteLine($"Name:{p.Name +p.ListPrice}");
-}
