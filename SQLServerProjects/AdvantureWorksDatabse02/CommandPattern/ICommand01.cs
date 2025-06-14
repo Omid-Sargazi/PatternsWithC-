224294460eb0964ad786959d6e23f32bb2c99296
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-14 13:14:07 +0330

    simple command in commnad pattern

--- SQLServerProjects/AdvantureWorksDatabse02/CommandPattern/ICommand01.cs
+++ SQLServerProjects/AdvantureWorksDatabse02/CommandPattern/ICommand01.cs
@@ -4,4 +4,17 @@ namespace AdvantureWorksDatabse02.CommandPattern
     {
         void Execute();
     }
+
+    public class SimpleCommand : ICommand01
+    {
+        private string _payload = string.Empty;
+        public SimpleCommand(string payload)
+        {
+            _payload = payload;
+        }
+        public void Execute()
+        {
+            Console.WriteLine($"SimpleCommand: See, I can do simple things like printing ({this._payload})");
+        }
+    }
 }
