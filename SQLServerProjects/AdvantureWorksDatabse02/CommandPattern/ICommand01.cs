6da440d96fce59b41e5c68e65478585e935dc575
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-14 13:21:20 +0330

    .

--- SQLServerProjects/AdvantureWorksDatabse02/CommandPattern/ICommand01.cs
+++ SQLServerProjects/AdvantureWorksDatabse02/CommandPattern/ICommand01.cs
@@ -17,4 +17,37 @@ namespace AdvantureWorksDatabse02.CommandPattern
             Console.WriteLine($"SimpleCommand: See, I can do simple things like printing ({this._payload})");
         }
     }
+
+    public class ComplexCommand : ICommand01
+    {
+        private string _a;
+        private string _b;
+        private Receiver _receiver;
+        public ComplexCommand(Receiver receiver, string a, string b)
+        {
+            _a = a;
+            _b = b;
+            _receiver = receiver;
+
+        }
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class Receiver
+    {
+        public void DoSomething(string a)
+        {
+            Console.WriteLine($"Receiver: Working on ({a}.)");
+        }
+
+        public void DoSomethingElse(string b)
+        {
+            Console.WriteLine($"Receiver: Also working on ({b}.)");
+        }
+    }
+
 }
