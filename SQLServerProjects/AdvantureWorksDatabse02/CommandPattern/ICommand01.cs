908f8e4658884f45444ec8cc0d053fe3b8b0db5b
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-14 18:45:08 +0330

    light controller by command pattern

--- SQLServerProjects/AdvantureWorksDatabse02/CommandPattern/ICommand01.cs
+++ SQLServerProjects/AdvantureWorksDatabse02/CommandPattern/ICommand01.cs
@@ -2,8 +2,8 @@ namespace AdvantureWorksDatabse02.CommandPattern
 {
     public class Light
     {
-        public void TurnOn()=>Console.WriteLine();
-        public void TurnOff()=>Console.WriteLine();
+        public void TurnOn() => Console.WriteLine();
+        public void TurnOff() => Console.WriteLine();
     }
 
     public class RemoteControl
@@ -25,9 +25,45 @@ namespace AdvantureWorksDatabse02.CommandPattern
         }
     }
 
-    public interface  ICommand
+    public interface ICommand
     {
         void Execute();
         void Undo();
+    }
+
+    public class LightOnCommand : ICommand
+    {
+        private Light _light;
+        public LightOnCommand(Light light)
+        {
+            _light = light;
+        }
+        public void Execute()
+        {
+            _light.TurnOn();
+        }
+
+        public void Undo()
+        {
+            _light.TurnOff();
+        }
+    }
+
+    public class LightOffCommand : ICommand
+    {
+        private Light _light;
+        public LightOffCommand(Light light)
+        {
+            _light = light;
+        }
+        public void Execute()
+        {
+            _light.TurnOff();
+        }
+
+        public void Undo()
+        {
+            _light.TurnOn();
+        }
     }
 }
