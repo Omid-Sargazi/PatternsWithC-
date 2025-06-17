0dcbf9ed208be71e5cd85b6f3d396e8bd69656c2
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-18 00:24:02 +0330

    use case

--- SQLServerProjects/AdvantureWorksDatabse02/CleanArchitecture/User.cs
+++ SQLServerProjects/AdvantureWorksDatabse02/CleanArchitecture/User.cs
@@ -4,11 +4,39 @@ namespace AdvantureWorksDatabse02.CleanArchitecture
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public bool IsValidEmail()
+        {
+            return Email.Contains("@");
+        }
+    }
+    public interface IUserRepository
+    {
+        User GetById(int id);
+    }
+    public class GetUserUseCase
+    {
+        private readonly IUserRepository _repository;
+
+        public GetUserUseCase(IUserRepository userRepository)
+        {
+            _repository = repository;
+        }
+
+        public User Exexute(int id)
+        {
+            return _repository.GetById(id);
+        }
     }
 
     public interface IUserService
     {
         User GetUserById(int id);
+    }
+
+    public class GetUserUseCase
+    {
+        
     }
 
     public class UserController
