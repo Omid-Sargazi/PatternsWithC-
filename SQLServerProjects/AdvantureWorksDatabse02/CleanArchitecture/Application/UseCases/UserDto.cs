204dfafa9e02ef8c8d7d97e5eea41f716293fc54
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-18 12:02:47 +0330

    create DTOs for user

---
+++ SQLServerProjects/AdvantureWorksDatabse02/CleanArchitecture/Application/UseCases/UserDto.cs
@@ -0,0 +1,29 @@
+namespace AdvantureWorksDatabse02.CleanArchitecture.Application.UseCases
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+    }
+
+     public class CreateUserDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+
+    public class UpdateUserDto
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+    }
+}
