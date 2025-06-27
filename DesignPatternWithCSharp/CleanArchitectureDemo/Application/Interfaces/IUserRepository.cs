6231e080a582f3bf67b9747e3f4e1cfec7525782
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-27 21:53:37 +0330

    IUserRepository in Application layer

---
+++ DesignPatternWithCSharp/CleanArchitectureDemo/Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+using Domain.Entities;
+namespace Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Save(User user);
+        bool EmailExists(string email);
+    }
+}
