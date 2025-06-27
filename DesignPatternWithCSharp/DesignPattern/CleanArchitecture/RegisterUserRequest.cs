5bd3d811f9bdd2e024946dc4959c0b036a3c6f31
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-27 20:26:14 +0330

    use input and output in clean archtecture

--- DesignPatternWithCSharp/DesignPattern/CleanArchitecture/RegisterUserRequest.cs
+++ DesignPatternWithCSharp/DesignPattern/CleanArchitecture/RegisterUserRequest.cs
@@ -20,28 +20,56 @@ namespace DesignPattern.CleanArchitecture
     public class RegisterUserUseCasee : IRegisterUserInput
     {
         private readonly IUserRepository _userRepository;
+        private readonly IRegisterUserOutput _presenter;
 
-        public RegisterUserUseCasee(IUserRepository userRepository)
+        public RegisterUserUseCasee(IUserRepository userRepository,
+        IRegisterUserOutput presenter)
         {
             _userRepository = userRepository;
+            _presenter = presenter;
         }
         public RegisterUserResponse Execute(RegisterUserRequest request)
         {
+            var response = new RegisterUserResponse();
             if (_userRepository.EmailExists(request.Email))
             {
-                return new RegisterUserResponse { Success = false, Message = "Email already exists" };
+                response.Success = false;
+                response.Message = "Email exists";
+                _presenter.Present(response);
+                return response;
+                // return new RegisterUserResponse { Success = false, Message = "Email already exists" };
             }
 
             var user = new User(request.Email, request.Password);
 
             if (!user.IsValid())
             {
-                return new RegisterUserResponse { Success = false, Message = "Invalid data" };
+                response.Success = false;
+                response.Message = "Invalid data.";
+                _presenter.Present(response);
+                return response;
+                // return new RegisterUserResponse { Success = false, Message = "Invalid data" };
             }
 
             _userRepository.Save(user);
+            response.Success = true;
+            response.Message = "User registered!";
+            _presenter.Present(response);
 
             return new RegisterUserResponse { Success = true, Message = "User registered!" };
+        }
+    }
+
+    public interface IRegisterUserOutput
+    {
+        void Present(RegisterUserResponse response);
+    }
+
+    public class ConsolePresenter : IRegisterUserOutput
+    {
+        public void Present(RegisterUserResponse response)
+        {
+            Console.WriteLine(response.Success ? "✅ " + response.Message : "❌ " + response.Message);
         }
     }
 }
