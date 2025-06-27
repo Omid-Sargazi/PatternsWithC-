092f4ac0bd674f5d2d08464812ece4d0e3370626
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-28 02:31:08 +0330

    create an entitie: User

---
+++ DesignPatternWithCSharp/CleanArchitectureOrders/Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+namespace Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User(string email, string password)
+        {
+            Id = Guid.NewGuid();
+            Email = email;
+            Password = password;
+        }
+        public bool IsValid()
+        {
+            return Email.Contains("@") && Password.Length >= 6;
+        }
+    }
+}
