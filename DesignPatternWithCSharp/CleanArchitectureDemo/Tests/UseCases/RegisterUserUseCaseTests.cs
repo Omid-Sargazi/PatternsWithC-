95e26e2edbcc4415128a0fedb30005309e6c0c46
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-28 00:25:05 +0330

    test for Should Fail When Email Is Already Registered

--- DesignPatternWithCSharp/CleanArchitectureDemo/Tests/UseCases/RegisterUserUseCaseTests.cs
+++ DesignPatternWithCSharp/CleanArchitectureDemo/Tests/UseCases/RegisterUserUseCaseTests.cs
@@ -20,8 +20,22 @@ namespace Tests.UseCases
 
             var result = useCase.Execute(request);
 
-            result.Success.Should().BeTrue();   
+            result.Success.Should().BeTrue();
             result.Message.Should().Be("User registered successfully.");
+        }
+        [Fact]
+        public void Should_Fail_When_Email_Is_Already_Registered()
+        {
+            var repo = new InMemoryUserRepository();
+            var useCase = new RegisterUserUseCase(repo);
+            var request01 = new RegisterUserRequest { Email = "duplicate@example.com", Password = "secure123" };
+            var request02 = new RegisterUserRequest { Email = "duplicate@example.com", Password = "Secure1234" };
+
+            useCase.Execute(request01);
+            var result = useCase.Execute(request02);
+
+            result.Success.Should().BeFalse();
+            result.Message.Should().Be("Email already registered!");
         }
     }
 }
