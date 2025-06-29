37a0b03d7249bc9067b4a19d203682888e157b82
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-29 21:49:00 +0330

    Observer Pattern for alert parice of stock

--- DesignPatternWithCSharp/DesignPattern/ObservePattern/Stock.cs
+++ DesignPatternWithCSharp/DesignPattern/ObservePattern/Stock.cs
@@ -1,17 +1,67 @@
 namespace DesignPattern.ObservePattern
 {
-    public class Stock
+
+    public interface IObserver
     {
-        public string Symbol { get; set; }
-        public decimal Price { get; set; }
+        void Upadte(decimal newPrice);
+    }
+    public interface IStock
+    {
+        void Attach(IObserver observer);
+        void Detach(IObserver observer);
+        void Notify();
+    }
 
-        public void ChangePrice(decimal newPrice)
+    public class Dashboard : IObserver
+    {
+        public void Upadte(decimal newPrice)
         {
-            Price = newPrice;
+            Console.WriteLine($"[Dashboard] Price updated: {newPrice}");
+        }
+    }
 
-            Console.WriteLine($"Dashboard updated for {Symbol}");
-            Console.WriteLine($"Log written for {Symbol}");
-            Console.WriteLine($"Alert checked for {Symbol}");
+    public class Logger : IObserver
+    {
+        public void Upadte(decimal newPrice)
+        {
+            Console.WriteLine($"[Logger] Logged price: {newPrice}");
+        }
+    }
+
+    public class AlertSystem : IObserver
+    {
+        public void Upadte(decimal newPrice)
+        {
+            if (newPrice > 150)
+                Console.WriteLine($"[Alert] Price too high: {newPrice}");
+        }
+    }
+
+    public class Stock : IStock
+    {
+        private List<IObserver> _observers = new List<IObserver>();
+
+        public string Symbol { get; set; }
+        public decimal price { get; set; }
+
+        public void Attach(IObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+       
+
+        public void Detach(IObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Upadte(price);
+            }
         }
     }
 }
