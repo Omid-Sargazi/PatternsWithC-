cbad60f4ff2f86bdc6d5030f01a5a80259b0fb93
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-29 22:36:52 +0330

    define all observers

--- DesignPatternWithCSharp/DesignPattern/ObservePattern/NotificationSystem.cs
+++ DesignPatternWithCSharp/DesignPattern/ObservePattern/NotificationSystem.cs
@@ -5,5 +5,27 @@ namespace DesignPattern.ObservePattern
         void OnUserRegister(string userName);
     }
 
-    
+    public class EmailNotifier : IUserObserver
+    {
+        public void OnUserRegister(string userName)
+        {
+            Console.WriteLine($"[Email] Welcome email sent to {userName}");
+        }
+    }
+
+    public class AdminNotifier : IUserObserver
+    {
+        public void OnUserRegister(string userName)
+        {
+                    Console.WriteLine($"[Admin Dashboard] Admin notified about {userName}");
+        }
+    }
+
+    public class Loggerr : IUserObserver
+    {
+        public void OnUserRegister(string userName)
+        {
+                    Console.WriteLine($"[Logger] User {userName} registered at {DateTime.Now}");
+        }
+    }
 }
