d6e92e70eb0c68132c726c037cd4d0e6379033fa
Omid-Sargazi <om.sargazi@gmail.com>
2025-06-30 23:46:09 +0330

    draw shapes with bridge pattern

--- DesignPatternWithCSharp/DesignPattern/BridgePattern/CircleOpenGL.cs
+++ DesignPatternWithCSharp/DesignPattern/BridgePattern/CircleOpenGL.cs
@@ -1,5 +1,73 @@
 namespace DesignPattern.BridgePattern
 {
+
+    public interface IDrawApi
+    {
+        void DrawCircle();
+        void DrawSquare();
+    }
+
+    public class OpenGLDrawer : IDrawApi
+    {
+        public void DrawCircle()
+        {
+            Console.WriteLine("Circle with OpenGL");
+        }
+
+        public void DrawSquare()
+        {
+            Console.WriteLine("Square with OpenGL");
+        }
+    }
+
+    public class DirectXDrawer : IDrawApi
+    {
+        public void DrawCircle()
+        {
+            Console.WriteLine("Circle with DirectX");
+        }
+
+        public void DrawSquare()
+        {
+            Console.WriteLine("Square with DirectX");
+        }
+    }
+
+    public abstract class Shape
+    {
+        protected IDrawApi _drawer;
+        public Shape(IDrawApi drawer)
+        {
+            _drawer = drawer;
+        }
+
+        public abstract void Draw();
+    }
+
+    public class Circle : Shape
+    {
+        public Circle(IDrawApi drawer) : base(drawer)
+        {
+        }
+
+        public override void Draw()
+        {
+            _drawer.DrawCircle();
+        }
+    }
+
+    public class Square : Shape
+    {
+        public Square(IDrawApi drawer) : base(drawer)
+        {
+        }
+
+        public override void Draw()
+        {
+            _drawer.DrawSquare();
+        }
+    }
+
     public class CircleOpenGL
     {
         public void Draw()
