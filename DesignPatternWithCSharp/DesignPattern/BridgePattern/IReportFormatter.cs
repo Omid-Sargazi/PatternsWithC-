becbeb703df164421f0db6366a831b15a2af60e1
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-01 19:22:15 +0330

    Created Excel report Fromat by Bridge pattern

--- DesignPatternWithCSharp/DesignPattern/BridgePattern/IReportFormatter.cs
+++ DesignPatternWithCSharp/DesignPattern/BridgePattern/IReportFormatter.cs
@@ -16,23 +16,48 @@ namespace DesignPattern.BridgePattern
 
         public void GenerateBody(string data)
         {
-             _content.AddRange(data.Select(item => $"PDF Data: {item}"));
+            _content.AddRange(data.Select(item => $"PDF Data: {item}"));
         }
 
         public void GenerateFooter(string footerText)
         {
-             _content.Add($"PDF Footer: {footerText}");
+            _content.Add($"PDF Footer: {footerText}");
         }
 
         public void GenerateHeader(string title)
         {
-             _content.Add($"PDF Header: {title}");
+            _content.Add($"PDF Header: {title}");
         }
 
         public byte[] GetFormatRepoter()
         {
             Console.WriteLine("Generating PDF report...");
             return Encoding.UTF8.GetBytes(string.Join(Environment.NewLine, _content));
+        }
+    }
+
+    public class ExcelReportFormatter : IReportFormatter
+    {
+        private readonly List<string> _content = new();
+        public void GenerateBody(string data)
+        {
+            _content.AddRange(data.Select(item => $"Excel Row: {item}"));
+        }
+
+        public void GenerateFooter(string footerText)
+        {
+             _content.Add($"Excel Footer: {footerText}");
+        }
+
+        public void GenerateHeader(string title)
+        {
+            _content.Add($"Excel Header: {title}");
+        }
+
+        public byte[] GetFormatRepoter()
+        {
+             Console.WriteLine("Generating Excel report...");
+             return Encoding.UTF8.GetBytes(string.Join("\t", _content));
         }
     }
 }
