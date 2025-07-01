15ded5a3304fcde378597bc800cf1072f257ab42
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-01 19:20:09 +0330

    created a pdf formatter by bridge pattern

---
+++ DesignPatternWithCSharp/DesignPattern/BridgePattern/IReportFormatter.cs
@@ -0,0 +1,38 @@
+using System.Text;
+
+namespace DesignPattern.BridgePattern
+{
+    public interface IReportFormatter
+    {
+        void GenerateHeader(string title);
+        void GenerateBody(string data);
+        void GenerateFooter(string footerText);
+        byte[] GetFormatRepoter();
+    }
+
+    public class PdfReportFormatter : IReportFormatter
+    {
+        private readonly List<string> _content = new List<string>();
+
+        public void GenerateBody(string data)
+        {
+             _content.AddRange(data.Select(item => $"PDF Data: {item}"));
+        }
+
+        public void GenerateFooter(string footerText)
+        {
+             _content.Add($"PDF Footer: {footerText}");
+        }
+
+        public void GenerateHeader(string title)
+        {
+             _content.Add($"PDF Header: {title}");
+        }
+
+        public byte[] GetFormatRepoter()
+        {
+            Console.WriteLine("Generating PDF report...");
+            return Encoding.UTF8.GetBytes(string.Join(Environment.NewLine, _content));
+        }
+    }
+}
