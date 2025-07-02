2b7a794e4d8d220039bd0a9f95bbea611bd59044
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-02 19:13:10 +0330

    .

---
+++ DesignPatternWithCSharp/DesignPattern/BridgePattern/ITelevision.cs
@@ -0,0 +1,82 @@
+using System.Formats.Tar;
+
+namespace DesignPattern.BridgePattern
+{
+    public interface ITelevision
+    {
+        void TurnOn();
+        void TurnOff();
+        void SetChannel(int channel);
+    }
+
+    public class SonyTV : ITelevision
+    {
+        public void SetChannel(int channel)
+        {
+            Console.WriteLine($"Sony TV channel set to {channel}");
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("Sony TV is now OFF");
+        }
+
+        public void TurnOn()
+        {
+            Console.WriteLine("Sony TV is now ON");
+        }
+    }
+
+    public class LGTV : ITelevision
+    {
+        public void SetChannel(int channel)
+        {
+            Console.WriteLine($"LG TV channel set to {channel}");
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("LG TV is now OFF");
+        }
+
+        public void TurnOn()
+        {
+            Console.WriteLine("LG TV is now ON");
+        }
+    }
+
+    public abstract class Remote
+    {
+        protected readonly ITelevision _television;
+        public Remote(ITelevision television)
+        {
+            _television = television;
+        }
+
+        public abstract void TurnOn();
+        public abstract void TurnOff();
+        public abstract void SetChannel(int channel);
+    }
+
+    public class BasicRemote : Remote
+    {
+        public BasicRemote(ITelevision television) : base(television)
+        {
+        }
+
+        public override void SetChannel(int channel)
+        {
+            _television.SetChannel(channel);
+        }
+
+        public override void TurnOff()
+        {
+            _television.TurnOff();
+        }
+
+        public override void TurnOn()
+        {
+            _television.TurnOn();
+        }
+    }
+}
