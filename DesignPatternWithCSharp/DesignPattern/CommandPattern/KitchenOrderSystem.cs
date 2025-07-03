a8bb606823bd8e0ac6b75e71aef1639bdd9b3ec6
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-03 12:06:34 +0330

    Commad Pattern for kitchen robot

--- DesignPatternWithCSharp/DesignPattern/CommandPattern/KitchenOrderSystem.cs
+++ DesignPatternWithCSharp/DesignPattern/CommandPattern/KitchenOrderSystem.cs
@@ -1,9 +1,57 @@
 namespace DesignPattern.CommandPattern
 {
+    public interface ICommandRobot
+    {
+        void Execute();
+    }
+
+    public class CookCommand : ICommandRobot
+    {
+        protected RobotChef _chef;
+
+        public CookCommand(RobotChef chef)
+        {
+            _chef = chef;
+        }
+
+        public void Execute()
+        {
+            _chef.Cook();
+        }
+    }
+
+    public class WashCommand : ICommandRobot
+    {
+        protected RobotChef _chef;
+        public WashCommand(RobotChef chef)
+        {
+            _chef = chef;
+        }
+        public void Execute()
+        {
+            _chef.Wash();
+        }
+    }
+
+    public class CleanCommand : ICommandRobot
+    {
+        protected RobotChef _chef;
+        public CleanCommand(RobotChef chef)
+        {
+            _chef = chef;
+        }
+        public void Execute()
+        {
+            _chef.Clean();
+        }
+    }
+
     public class RobotChef
     {
         public void Cook() => Console.WriteLine("Cooking....");
         public void Wash() => Console.WriteLine("Washing dishes....");
         public void Clean() => Console.WriteLine("Cleaning floor....");
     }
+
+   
 }
