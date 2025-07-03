23984db765fb8b746ff564f8581149e68183bee4
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-03 11:57:49 +0330

    Kitchen Robot without Command  Pattern

---
+++ DesignPatternWithCSharp/DesignPattern/CommandPattern/KitchenOrderSystem.cs
@@ -0,0 +1,9 @@
+namespace DesignPattern.CommandPattern
+{
+    public class RobotChef
+    {
+        public void Cook() => Console.WriteLine("Cooking....");
+        public void Wash() => Console.WriteLine("Washing dishes....");
+        public void Clean() => Console.WriteLine("Cleaning floor....");
+    }
+}
