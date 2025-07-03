b997b67ef780094dbde204433f09ba5d8bec4484
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-03 19:54:51 +0330

    Define an Interface for Mediator Pattern

--- DesignPatternWithCSharp/DesignPattern/MediatorPattern/User.cs
+++ DesignPatternWithCSharp/DesignPattern/MediatorPattern/User.cs
@@ -1,5 +1,20 @@
 namespace DesignPattern.MediatorPattern
 {
+
+    public interface IChatRoom
+    {
+        void ShowMessage(string message, User sender, User receiver);
+    }
+
+    public class ChatRoom : IChatRoom
+    {
+        public void ShowMessage(string message, User sender, User receiver)
+        {
+            Console.WriteLine($"{sender.Name} to {receiver.Name}: {message}");
+            receiver.ReceiveMessage(message, sender);
+        }
+    }
+
     public class User
     {
         public string Name { get; set; }
