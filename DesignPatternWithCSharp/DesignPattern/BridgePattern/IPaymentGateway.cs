1bfacec26edb9a9ee452e8bebd7ade2c7ac90ee3
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-06 00:15:18 +0330

    .

---
+++ DesignPatternWithCSharp/DesignPattern/BridgePattern/IPaymentGateway.cs
@@ -0,0 +1,32 @@
+namespace DesignPattern.BridgePattern
+{
+    public interface IPaymentGateway
+    {
+        void Process(decimal amount);
+    }
+
+    public class StripeGateway : IPaymentGateway
+    {
+        public void Process(decimal amount)
+    {
+        Console.WriteLine($"Processing payment of {amount} via Stripe.");
+    }
+    }
+
+
+    public class LocalBankGateway : IPaymentGateway
+    {
+        public void Process(decimal amount)
+    {
+        Console.WriteLine($"Processing payment of {amount} via LocalBank.");
+    }
+    }
+
+    public class PayPalGateway  :IPaymentGateway
+    {
+        public void Process(decimal amount)
+    {
+        Console.WriteLine($"Processing payment of {amount} via PayPal Gateway.");
+    }
+    }
+}
