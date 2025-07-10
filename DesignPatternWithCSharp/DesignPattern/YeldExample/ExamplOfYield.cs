7ced16e3db298a6fd8654abcab78064aad2d278b
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-10 12:15:09 +0330

    .

--- DesignPatternWithCSharp/DesignPattern/YeldExample/ExamplOfYield.cs
+++ DesignPatternWithCSharp/DesignPattern/YeldExample/ExamplOfYield.cs
@@ -9,7 +9,12 @@ namespace DesignPattern.YeldExample
             {
                 Console.WriteLine($"{item}: Yileld");
             }
-        } 
+
+            foreach (var item in GetEventNumber(10))
+            {
+                Console.WriteLine($"{item} : event number by Yield");
+            }
+        }
 
 
         static IEnumerable<int> GetNumbers()
@@ -17,6 +22,14 @@ namespace DesignPattern.YeldExample
             yield return 1;
             yield return 2;
             yield return 3;
+        }
+
+        static IEnumerable<int> GetEventNumber(int max)
+        {
+            for (int i = 0; i <= max; i+=2)
+            {
+                yield return i;
+            }
         }
     }
 }
