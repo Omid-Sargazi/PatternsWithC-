9f46a087af0a63c4b17ccbc6777801e883448e4e
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-12 22:09:42 +0330

    .

---
+++ DesignPatternWithCSharp/DesignPattern/ChainOfResponsibility/HttpRequest.cs
@@ -0,0 +1,44 @@
+namespace DesignPattern.ChainOfResponsibility
+{
+    public enum FirewallResult
+    {
+        Allowed,
+        BlockeByIP,
+        BlockeByPath,
+        BlockedByratelimit,
+    };
+
+    public delegate FirewallResult FirewallCheck(HttpRequest request);
+
+    public class Firewall
+    {
+        private readonly List<FirewallCheck> _handlers = new();
+        public Firewall Addhandler(FirewallCheck handler)
+        {
+            _handlers.Add(handler);
+            return this;
+        }
+
+        public FirewallResult Handle(HttpRequest request)
+        {
+            foreach (var handler in _handlers)
+            {
+                var result = handler(request);
+                if (result != FirewallResult.Allowed)
+                {
+                    return result;
+                }
+            }
+            return FirewallResult.Allowed;
+        }
+    }
+
+    public class HttpRequest
+    {
+        public string IP { get; set; }
+        public string Path { get; set; }
+        public int RequestsPerMinute { get; set; }
+    }
+
+
+}
