f6576fdfe839b6501650de7e97bee726ead78547
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-12 18:23:52 +0330

    improve chain of responsibility by Func and delegate

--- DesignPatternWithCSharp/DesignPattern/ChainOfResponsibility/LeaveHandler.cs
+++ DesignPatternWithCSharp/DesignPattern/ChainOfResponsibility/LeaveHandler.cs
@@ -1,3 +1,5 @@
+using System.Linq.Expressions;
+
 namespace DesignPattern.ChainOfResponsibility
 {
      public enum LeaveReason
@@ -16,6 +18,33 @@ namespace DesignPattern.ChainOfResponsibility
     }
     public class LeaveHandler
     {
-        
+        private readonly Func<LeaveRequestt, bool> _canHandle;
+        private readonly string _handlerName;
+        private LeaveHandler _next;
+
+        public LeaveHandler(string handlerName, Func<LeaveRequestt, bool> canHandle)
+        {
+            _handlerName = handlerName;
+            _canHandle = canHandle;
+        }
+
+        public LeaveHandler SetNext(LeaveHandler nexHandler)
+        {
+            _next = nexHandler;
+            return nexHandler;
+        }
+
+
+        public void Handle(LeaveRequestt request)
+        {
+            if (_canHandle(request))
+                Console.WriteLine($"✅ {_handlerName} approved {request.EmployeeName}'s leave for {request.NumberOfDays} days. Reason: {request.Reason}");
+            else if (_next != null)
+                _next.Handle(request);
+            else
+            {
+                Console.WriteLine($"❌ No handler could approve {request.EmployeeName}'s leave request.");
+            }
+        }
     }
 }
