edd3c0296fffd1b7a57d380684aaec94a0e8a93a
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-12 17:38:57 +0330

    define a chain of classes

--- DesignPatternWithCSharp/DesignPattern/ChainOfResponsibility/Example1.cs
+++ DesignPatternWithCSharp/DesignPattern/ChainOfResponsibility/Example1.cs
@@ -10,4 +10,37 @@ namespace DesignPattern.ChainOfResponsibility
 
         public abstract void Handle(string requestType);
     }
+
+    public class Level1Support : SupportHandler
+    {
+        public override void Handle(string requestType)
+        {
+            if (requestType == "General")
+                Console.WriteLine($"Technical Engineer handle the request.");
+            else if (_next != null)
+                _next.Handle(requestType);
+        }
+    }
+
+    public class TechnicalSupport : SupportHandler
+    {
+        public override void Handle(string requestType)
+        {
+            if (requestType == "Technical")
+                Console.WriteLine($"Technical Engineer handled the request");
+            else if (_next != null)
+                _next.Handle(requestType);
+        }
+    }
+
+    public class ManagerSupport : SupportHandler
+    {
+        public override void Handle(string requestType)
+        {
+            if (requestType == "Complaint")
+                Console.WriteLine($"Manager Handle the request.");
+            else
+                Console.WriteLine("Unknown request. No handler could process it.");
+        }
+    }
 }
