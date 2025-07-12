f859602241a8f0b5efaffd35fda75b4f15d4d3d5
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-12 18:12:48 +0330

    .

--- DesignPatternWithCSharp/DesignPattern/ChainOfResponsibility/LeaveRequest.cs
+++ DesignPatternWithCSharp/DesignPattern/ChainOfResponsibility/LeaveRequest.cs
@@ -15,15 +15,46 @@ namespace DesignPattern.ChainOfResponsibility
             _next = nextHandler;
         }
 
-        public abstract void Handler(LeaveRequest request);
+        public abstract void Handle(LeaveRequest request);
     }
 
     public class TeamLeadHandler : LeaveRequestHandler
     {
-        public override void Handler(LeaveRequest request)
+        public override void Handle(LeaveRequest request)
         {
-            throw new NotImplementedException();
+            if (request.NumberOfDays <= 2 && request.Reason.ToLower() != "medical")
+                Console.WriteLine($"✅ TeamLead approved {request.EmployeeName}'s leave for {request.NumberOfDays} days.");
+            else
+            {
+                _next?.Handle(request);
+            }
         }
     }
 
+    public class ProjectManagerHandler : LeaveRequestHandler
+    {
+        public override void Handle(LeaveRequest request)
+        {
+            if (request.NumberOfDays <= 5 && request.Reason.ToLower() != "medical")
+                Console.WriteLine($"✅ TeamLead approved {request.EmployeeName}'s leave for {request.NumberOfDays} days.");
+            else
+            {
+                _next?.Handle(request);
+            }
+        }
+    }
+
+    public class HRManagerHandler : LeaveRequestHandler
+    {
+        public override void Handle(LeaveRequest request)
+        {
+            if (request.NumberOfDays > 5 || request.Reason.ToLower() == "medical")
+                Console.WriteLine($"✅ HRManager approved {request.EmployeeName}'s leave for {request.NumberOfDays} days. Reason: {request.Reason}");
+
+            else
+            {
+                _next?.Handle(request);
+            }
+        }
+    }
 }
