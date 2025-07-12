17f191d92144c22ed9d2f67de84b38f3fe648289
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-12 22:53:59 +0330

    .

--- DesignPatternWithCSharp/DesignPattern/ChainOfResponsibility/Resume.cs
+++ DesignPatternWithCSharp/DesignPattern/ChainOfResponsibility/Resume.cs
@@ -15,4 +15,30 @@ namespace DesignPattern.ChainOfResponsibility
         RejectedByEnglishLevel,
         RequiresManagerApproval
     }
+    public delegate ResumeStatus? ResumeCheck(Resume resume);
+
+    public class ResumeProcessor
+    {
+        private readonly List<ResumeCheck> _checks = new();
+
+        public ResumeProcessor AddCheck(ResumeCheck check)
+        {
+            _checks.Add(check);
+            return this;
+        }
+
+        public ResumeStatus Process(Resume resume)
+        {
+            foreach (var check in _checks)
+            {
+                var result = check(resume);
+                if (result.HasValue && result.Value != ResumeStatus.Accepted)
+                {
+                    return result.Value;
+                }
+            }
+
+            return ResumeStatus.Accepted;
+        }
+    }
 }
