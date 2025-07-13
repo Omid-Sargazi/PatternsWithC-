f6bac90be1966b27875f905a58b93b74f6f22bfc
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-13 22:52:21 +0330

    usage of the pattern

--- DesignPatternWithCSharp/LeetCode/LeetCode/Program.cs
+++ DesignPatternWithCSharp/LeetCode/LeetCode/Program.cs
@@ -1,4 +1,5 @@
-﻿using LeetCode.Problem1;
+﻿using LeetCode.Patterns.ChainOfResponsibility;
+using LeetCode.Problem1;
 
 namespace LeetCode
 {
@@ -8,7 +9,7 @@ namespace LeetCode
         {
             Console.WriteLine("Hello, World!");
 
-            //===========================
+            //========================================
             var twoSum = new TwoSum();
             int[] nums = new int[] { 2, 12, 7, 15 };
             int target = 9;
@@ -17,7 +18,35 @@ namespace LeetCode
             {
                 Console.WriteLine($"Locations of numbers are {resultTwoSum[i]}");
             }
-            //===========================
+            //========================================
+
+
+            //========================================
+            var transaction = new Transaction
+            {
+                UserName = "Sara",
+                Amount = 900_000,
+                Balance = 800_000,
+                DailyLimit = 700_000,
+                IsBlacklisted = true
+            };
+
+            var handkerChain = HandlerBuilder.BuildChain();
+            var resultt = new TransactionResult();
+
+            Console.WriteLine($"the end of the result");
+            if (resultt.IsSuccess)
+            {
+                Console.WriteLine("Transaction is Ok");
+            }
+            else
+            {
+                foreach (var err in resultt.Errors)
+                {
+                    Console.WriteLine(err);
+                }
+            }
+            //========================================
         }
     }
 }
