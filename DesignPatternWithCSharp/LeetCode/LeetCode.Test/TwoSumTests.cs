73868eb8015d461b0867d7b59b1692fbcab94cd7
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-13 19:45:27 +0330

    Test of Edge Cases

--- DesignPatternWithCSharp/LeetCode/LeetCode.Test/TwoSumTests.cs
+++ DesignPatternWithCSharp/LeetCode/LeetCode.Test/TwoSumTests.cs
@@ -11,4 +11,21 @@ public class TwoSumTests
         int[] result = twoSum.TwoSumRun(new int[] { 2, 7, 11, 15 }, 9);
         Assert.Equal(new int[] { 0, 1 }, result);
     }
+
+    [Fact]
+    public void HandlesDuplicateNumbers()
+    {
+        var twoSum = new TwoSum();
+        int[] result = twoSum.TwoSumRun(new int[] { 3, 3 }, 6);
+        Assert.Equal(new int[] { 0, 1 }, result);
+    }
+
+    [Fact]
+    public void ReturnsEmptyArray_WhenNoSolution()
+    {
+        var solution = new TwoSum();
+        int[] result = solution.TwoSumRun(new int[] { 1, 2, 3 }, 10);
+        Assert.Equal(new int[] { }, result);
+    }
+
 }
