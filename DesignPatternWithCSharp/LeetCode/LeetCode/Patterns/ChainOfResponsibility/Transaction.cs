276c85805e1729e5339335dce2ed6669e37038b2
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-13 22:39:00 +0330

    Abstarct Class for the pattern

--- DesignPatternWithCSharp/LeetCode/LeetCode/Patterns/ChainOfResponsibility/Transaction.cs
+++ DesignPatternWithCSharp/LeetCode/LeetCode/Patterns/ChainOfResponsibility/Transaction.cs
@@ -15,5 +15,27 @@ namespace LeetCode.Patterns.ChainOfResponsibility
         public bool IsSuccess => Errors.Count == 0;
     }
 
-    
+    public abstract class TransactionHandler
+    {
+        protected TransactionHandler _next;
+        public void SetNext(TransactionHandler next)
+        {
+            _next = next;
+        }
+
+        public void Execute(Transaction transaction, TransactionResult result)
+        {
+            Console.WriteLine($"[{this.GetType().Name}] is checking...");
+            Handle(transaction, result);
+
+            _next?.Execute(transaction, result);
+
+        }
+
+        protected abstract void Handle(Transaction transaction, TransactionResult result);
+
+
+    }
+
+
 }
