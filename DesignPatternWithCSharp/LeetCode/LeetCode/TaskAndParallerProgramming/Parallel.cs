1cfa842204b3bbf81e411bcb0a793e4309696ca3
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-14 18:59:03 +0330

    .

--- DesignPatternWithCSharp/LeetCode/LeetCode/TaskAndParallerProgramming/Parallel.cs
+++ DesignPatternWithCSharp/LeetCode/LeetCode/TaskAndParallerProgramming/Parallel.cs
@@ -18,14 +18,35 @@ namespace LeetCode.TaskAndParallerProgramming
             Console.WriteLine($"{t2.Status}: Status of Tasks");
 
 
-            await Task.Run(async() =>
+            await Task.Run(async () =>
              {
                  for (int i = 0; i <= 5; i++)
                  {
                      Console.WriteLine($"Task is :{i}");
-                     await Task.Delay(1000);
+                     await Task.Delay(3000);
                  }
              });
+
+            var printNumbersTask = Task.Run(async () =>
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine($"Number: {i}");
+                    await Task.Delay(2000);
+                }
+            });
+
+            var parallelTask = Task.Run(async () =>
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"Parallel work: {i}");
+                    await Task.Delay(500);
+                }
+            });
+
+            await Task.WhenAll(printNumbersTask, parallelTask);
+            Console.WriteLine("All tasks completed!");
 
         }
     }
