c35ca09362f36758f5c2cc294bd97e12f0ad6bc1
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-17 00:03:23 +0330

    APIs for getting Products

--- DesignPatternWithCSharp/AdventureWorksAPI/AdventureWorksAPI/Controllers/ProductsController.cs
+++ DesignPatternWithCSharp/AdventureWorksAPI/AdventureWorksAPI/Controllers/ProductsController.cs
@@ -19,5 +19,32 @@ namespace AdventureWorksAPI.Controllers
             var result = await _repository.GetTop10ProductsAsync();
             return Ok(result);
         }
+        [HttpGet("over1000")]
+        public async Task<IActionResult> GetProductsOver1000()
+        {
+            var result = await _repository.GetProductsOver1000Async();
+            return Ok(result);
+        }
+
+        [HttpGet("price-desc")]
+        public async Task<IActionResult> GetProductsOrderedByPriceDesc()
+        {
+            var result = await _repository.GetOrderedByPriceDescAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("total-count")]
+        public async Task<IActionResult> GetTotalProductCount()
+        {
+            var result = await _repository.GetTotalProductCountAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("with-subcategory")]
+        public async Task<IActionResult> GetProductsWithSubcategory()
+        {
+            var result = await _repository.GetProductsWithSubcategoryAsync();
+            return Ok(result);
+        }
     }
 }
