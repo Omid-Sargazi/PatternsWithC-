43cf69e01b2e06cebb8a89a8d33906572541fa6e
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-17 00:27:00 +0330

    Tets of GetTop10 Returns Ok Result With List Of Products

---
+++ DesignPatternWithCSharp/AdventureWorksAPI/AdventureWorksAPI.Tests/Controllers/ProductsControllerTests.cs
@@ -0,0 +1,38 @@
+using AdventureWorksAPI.Controllers;
+using AdventureWorksAPI.DTOs;
+using AdventureWorksAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace AdventureWorksAPI.Tests.Controllers
+{
+    public class ProductsControllerTests
+    {
+        private readonly Mock<IProductRepository> __mockRepo;
+        private readonly ProductsController _controller;
+        public ProductsControllerTests()
+        {
+            __mockRepo = new Mock<IProductRepository>();
+            _controller = new ProductsController(__mockRepo.Object);
+        }
+
+        [Fact]
+        public async Task GetTop10_ReturnsOkResult_WithListOfProducts()
+        {
+            var mockProducts = new List<ProductDto>
+            {
+                 new ProductDto { Name = "Bike", ProductNumber = "BK-001", ListPrice = 1000 },
+                new ProductDto { Name = "Helmet", ProductNumber = "HL-002", ListPrice = 200 }
+            };
+
+            __mockRepo.Setup(r => r.GetTop10ProductsAsync())
+            .ReturnsAsync(mockProducts);
+
+            var result = await _controller.GetTop10();
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var products = Assert.IsAssignableFrom<IEnumerable<ProductDto>>(okResult.Value);
+            Assert.Equal(2, ((List<ProductDto>)products).Count);
+        }
+    }
+}
