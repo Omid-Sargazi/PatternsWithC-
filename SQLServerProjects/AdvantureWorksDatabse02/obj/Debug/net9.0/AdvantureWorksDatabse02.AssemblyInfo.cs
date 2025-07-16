462ddefccaf159bd1ed67cca12bd33175aa8cef1
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-16 11:41:35 +0330

    .

--- SQLServerProjects/AdvantureWorksDatabse02/obj/Debug/net9.0/AdvantureWorksDatabse02.AssemblyInfo.cs
+++ SQLServerProjects/AdvantureWorksDatabse02/obj/Debug/net9.0/AdvantureWorksDatabse02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvantureWorksDatabse02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+204dfafa9e02ef8c8d7d97e5eea41f716293fc54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee5e9b348ffbe0071c1585fb986e092635b24c2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvantureWorksDatabse02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvantureWorksDatabse02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
