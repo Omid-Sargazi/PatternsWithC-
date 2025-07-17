fc62273a48f55265e71caa85bd5a807a508f4443
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-17 13:31:35 +0330

    Report Repository

--- DesignPatternWithCSharp/AdventureWorksAPI/AdventureWorksAPI/Repositories/IReportRepository.cs
+++ DesignPatternWithCSharp/AdventureWorksAPI/AdventureWorksAPI/Repositories/IReportRepository.cs
@@ -1,7 +1,13 @@
+using AdventureWorksAPI.DTOs;
+
 namespace AdventureWorksAPI.Repositories
 {
     public interface IReportRepository
     {
-        
+        Task<IEnumerable<CategoryProductCountDto>> GetProductCountsPerCategoryAsync();
+        Task<IEnumerable<CustomerSpendingDto>> GetTotalSpentPerCustomerAsync();
+        Task<IEnumerable<ProductNoOrderDto>> GetProductWithNoOrderAsync();
+        Task<IEnumerable<ProductAvgQtyDto>> GetAvgOrderQtyPerProductAsync();
+        Task<IEnumerable<TopOrderDto>> GetTop10OrdersAsync();
     }
 }
