076f9fbe850176c7aa3d7ad36be1debaec7ef806
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-17 12:35:40 +0330

    create contoller for getting top 5 customers

---
+++ DesignPatternWithCSharp/AdventureWorksAPI/AdventureWorksAPI/Controllers/CustomersController.cs
@@ -0,0 +1,24 @@
+using AdventureWorksAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AdventureWorksAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ICustomerRepository _repository;
+        public CustomersController(ICustomerRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("top5")]
+        public async Task<IActionResult> GetTop5()
+        {
+            var customers = await _repository.GetTop5CustomersAsync();
+            return Ok(customers);
+        }
+    }
+}
--- DesignPatternWithCSharp/AdventureWorksAPI/AdventureWorksAPI/Program.cs
+++ DesignPatternWithCSharp/AdventureWorksAPI/AdventureWorksAPI/Program.cs
@@ -8,7 +8,8 @@ builder.Services.AddDbContext<AdventureWorks2019Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
-builder.Services.AddScoped<IProductRepository, ProductRepository>();    
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 
 
 builder.Services.AddControllers();
