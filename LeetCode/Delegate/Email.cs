31dcbe7181bc8a3eae431b72e6ca484dc95b4d04
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-21 23:01:59 +0330

    Chain Of Responsibility by Fluent

--- LeetCode/Delegate/Email.cs
+++ LeetCode/Delegate/Email.cs
@@ -1,5 +1,28 @@
 namespace LeetCode.Delegate
 {
+    public delegate bool EmailRule(Email email);
+
+    public class EmailHandlerr
+    {
+        private readonly List<EmailRule> _rules = new();
+        public EmailHandlerr AddRule(EmailRule rule)
+        {
+            _rules.Add(rule);
+            return this;
+        }
+
+        public bool Handle(Email email)
+        {
+            foreach (var rule in _rules)
+            {
+                if (!rule(email))
+                    Console.WriteLine("Email Rejected");
+                return false;
+            }
+            Console.WriteLine($"Email Accepted");
+            return true;
+        }
+    }
     public class Email
     {
         public string Subject { get; set; }
@@ -35,13 +58,23 @@ namespace LeetCode.Delegate
         {
             var email1 = new Email { Subject = "buy now spam", Body = "short" };
             var email2 = new Email { Subject = "hello", Body = "this is a real message" };
-            var handler = new EmailHandler();
 
-            handler.AddFiletr(e => !e.Subject.Contains("spam"));
-            handler.AddFiletr(e => e.Body.Length > 20);
-
+            var handler = new EmailHandlerr()
+            .AddRule(e => e.Subject.ToLower().Contains("spam"))
+            .AddRule(e => e.Body.Length >= 20)
+            .AddRule(e => e.Body.Contains("Invoice") || e.Subject.Contains("invoice"));
             handler.Handle(email1);
             handler.Handle(email2);
+            // var handler = new EmailHandler();
+
+            // handler.AddFiletr(e => !e.Subject.Contains("spam"));
+            // handler.AddFiletr(e => e.Body.Length > 20);
+
+            // handler.Handle(email1);
+            // handler.Handle(email2);
         }
     }
+
+    
+
 } 
