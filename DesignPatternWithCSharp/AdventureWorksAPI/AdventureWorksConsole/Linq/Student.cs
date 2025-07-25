233c3c15b49922955eab120649aa1c062bd5c46d
Omid-Sargazi <om.sargazi@gmail.com>
2025-07-25 17:03:05 +0330

    SeelctMany for flatting nested lists

---
+++ DesignPatternWithCSharp/AdventureWorksAPI/AdventureWorksConsole/Linq/Student.cs
@@ -0,0 +1,57 @@
+namespace AdventureWorksConsole.Linq
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public List<string> Courses { get; set; }
+    }
+
+    public class RunLinq
+    {
+        public static void Run()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student{Name="Omid",Courses = new List<string>{"Math","Programming"}},
+                new Student{Name = "Saedd",Courses = new List<string> {"C#","C++"}}
+            };
+
+            var allCourses = students.Select(x => x.Courses);
+
+
+            List<string> colors = new List<string> { "Green", "Red" };
+            List<string> sizes = new List<string> { "S", "X" };
+
+            var combination = colors.SelectMany(color => sizes,
+            (color, size) => $"{color},{size}");
+
+
+            TreeNode root = new TreeNode
+            {
+                values = "Root",
+                Children = {
+                    new TreeNode{values="child1"},
+                    new TreeNode
+                    {
+                        values="child2",
+                        Children = {new TreeNode{values="Grandchild"}}
+                    }
+                }
+            };
+        }
+
+
+        IEnumerable<string> GetAllValues(TreeNode node)
+        {
+            return new[] { node.values }.Concat(node.Children.SelectMany(GetAllValues));
+        }
+    }
+
+
+    public class TreeNode
+    {
+        public string values { get; set; }
+        public List<TreeNode> Children = new List<TreeNode>();
+
+    }
+}
